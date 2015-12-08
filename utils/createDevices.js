69059e5c49abe42f441482678e1dbcf973cefb87
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2015-12-08 02:40:15 +0100

    Add util to generate the token
    This utility generates the token from tokenSecret configured
    in the config dir - i.e. the same secret that is used in the app.
    
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

---
+++ utils/createDevices.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) Mainflux
+ *
+ * mainflux-http-client-node is licensed under an Apache license, version 2.0 license.
+ * All rights not explicitly granted in the Apache license, version 2.0 are reserved.
+ * See the included LICENSE file for more details.
+ */
+
+var request = require('request');
+
+var mfHost = "http://localhost:8080";
+
+
