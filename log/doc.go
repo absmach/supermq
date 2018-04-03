8ae30ff5067ad474a14750b781e74c4c28d1a15e
Aleksandar Novakovic <anovakovic01@gmail.com>
2018-04-03 14:33:10 +0200

    Refactor logger and replace go-kit logger with wrapper
    Replace go-kit logger from services with logger wrapper. Refactor
    code in logger wrapper. Remove unnecessary subpackage, methods and
    log levels.
    
    Signed-off-by: Aleksandar Novakovic <anovakovic01@gmail.com>

--- log/doc.go
+++
@@ -1,3 +0,0 @@
-// Package log contains logger API definition, wrapper that
-// can be used around any other logger.
-package log
--- logger/level.go
+++ logger/level.go
@@ -1,29 +1,22 @@
-package log
+package logger
 
 const (
-	// All level is used when logging everything.
-	All Level = iota
 	// Error level is used when logging errors.
-	Error
+	Error Level = iota + 1
 	// Warn level is used when logging warnings.
 	Warn
 	// Info level is used when logging info data.
 	Info
 )
 
+// Level represents severity level while logging.
+type Level int
+
 var levels = map[Level]string{
 	Error: "error",
 	Warn:  "warn",
 	Info:  "info",
 }
-
-// AddLevel adds custom level's name to levels map.
-func AddLevel(lvl Level, name string) {
-	levels[lvl] = name
-}
-
-// Level represents severity level while logging.
-type Level int
 
 func (lvl Level) String() string {
 	return levels[lvl]
