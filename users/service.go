28571ed8cd4c3bb60d214bbbb8eedbbe32576e77
Aleksandar Novakovic <anovakovic01@gmail.com>
2018-04-26 10:13:40 +0200

    Add users service interfaces and domain logic
    Add interfaces for users service, hasher, identity provider and
    repository. Add users service domain logic.
    
    Signed-off-by: Aleksandar Novakovic <anovakovic01@gmail.com>

---
+++ users/service.go
@@ -0,0 +1,31 @@
+package users
+
+import "errors"
+
+var (
+	// ErrMalformedEntity indicates malformed entity specification (e.g.
+	// invalid username or password).
+	ErrMalformedEntity = errors.New("malformed entity specification")
+
+	// ErrUnauthorizedAccess indicates missing or invalid credentials provided
+	// when accessing a protected resource.
+	ErrUnauthorizedAccess = errors.New("missing or invalid credentials provided")
+)
+
+// Service specifies an API that must be fullfiled by the domain service
+// implementation, and all of its decorators (e.g. logging & metrics).
+type Service interface {
+	// Register creates new user account. In case of the failed registration, a
+	// non-nil error value is returned.
+	Register(User) error
+
+	// Login authenticates the user given its credentials. Successful
+	// authentication generates new access token. Failed invocations are
+	// identified by the non-nil error values in the response.
+	Login(User) (string, error)
+
+	// Identify validates user's token. If token is valid, user's id
+	// is returned. If token is invalid, or invocation failed for some
+	// other reason, non-nil error values are returned in response.
+	Identify(string) (string, error)
+}
