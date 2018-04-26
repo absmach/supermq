919e22db85636524d7fc44d17029df7c52791e72
Aleksandar Novakovic <anovakovic01@gmail.com>
2018-04-26 12:16:59 +0200

    Add users repository implementation
    Add users repository implementation utilising postgres.
    
    Signed-off-by: Aleksandar Novakovic <anovakovic01@gmail.com>

--- users/service.go
+++ users/service.go
@@ -3,6 +3,10 @@ package users
 import "errors"
 
 var (
+	// ErrConflict indicates usage of the existing email during account
+	// registration.
+	ErrConflict = errors.New("email already taken")
+
 	// ErrMalformedEntity indicates malformed entity specification (e.g.
 	// invalid username or password).
 	ErrMalformedEntity = errors.New("malformed entity specification")
@@ -10,6 +14,9 @@ var (
 	// ErrUnauthorizedAccess indicates missing or invalid credentials provided
 	// when accessing a protected resource.
 	ErrUnauthorizedAccess = errors.New("missing or invalid credentials provided")
+
+	// ErrNotFound indicates a non-existent entity request.
+	ErrNotFound = errors.New("non-existent entity")
 )
 
 // Service specifies an API that must be fullfiled by the domain service
