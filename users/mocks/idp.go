05a7e78e37ccca42726d489e645f497e5c561763
Aleksandar Novakovic <anovakovic01@gmail.com>
2018-05-04 17:04:51 +0200

    Split security file on hasher and idp files
    Signed-off-by: Aleksandar Novakovic <anovakovic01@gmail.com>

---
+++ users/mocks/idp.go
@@ -0,0 +1,29 @@
+package mocks
+
+import "github.com/mainflux/mainflux/users"
+
+var _ users.IdentityProvider = (*identityProviderMock)(nil)
+
+type identityProviderMock struct{}
+
+// NewIdentityProvider creates "mirror" identity provider, i.e. generated
+// token will hold value provided by the caller.
+func NewIdentityProvider() users.IdentityProvider {
+	return &identityProviderMock{}
+}
+
+func (idp *identityProviderMock) TemporaryKey(id string) (string, error) {
+	if id == "" {
+		return "", users.ErrUnauthorizedAccess
+	}
+
+	return id, nil
+}
+
+func (idp *identityProviderMock) PermanentKey(id string) (string, error) {
+	return idp.TemporaryKey(id)
+}
+
+func (idp *identityProviderMock) Identity(key string) (string, error) {
+	return idp.TemporaryKey(key)
+}
