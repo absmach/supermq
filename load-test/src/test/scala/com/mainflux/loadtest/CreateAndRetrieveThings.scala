6f01b9d87dd9390e9d1774bcf819093fee773b27
Dejan Mijic <dejan@mainflux.com>
2018-05-17 11:52:05 +0200

    Extract test case building blocks
    Signed-off-by: Dejan Mijic <dejan@mainflux.com>

--- load-test/src/test/scala/com/mainflux/loadtest/CreateAndRetrieveThings.scala
+++ load-test/src/test/scala/com/mainflux/loadtest/CreateAndRetrieveThings.scala
@@ -1,60 +1,30 @@
 package com.mainflux.loadtest
 
-import com.mainflux.loadtest.Constants._
-import io.circe._
-import io.circe.parser._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.http.protocol.HttpProtocolBuilder.toHttpProtocol
 import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
-import scalaj.http.Http
 
 import scala.concurrent.duration._
 
-final class CreateAndRetrieveThings extends Simulation {
-  import CreateAndRetrieveThings._
+final class CreateAndRetrieveThings extends TestCase {
+  override def prepareAndExecute(): SetUp = {
+    val token = authenticate()
+    val thing = """{"type":"device", "name":"weio"}"""
 
-  Http(s"$UsersURL/users")
-    .postData(User)
-    .header(HttpHeaderNames.ContentType, ContentType)
-    .asString
+    val scn = scenario("create and retrieve things")
+      .exec(http("create thing")
+        .post("/clients")
+        .header(HttpHeaderNames.ContentType, jsonType)
+        .header(HttpHeaderNames.Authorization, token)
+        .body(StringBody(thing))
+        .check(status.is(201))
+        .check(headerRegex(HttpHeaderNames.Location, "(.*)").saveAs("location")))
+      .exec(http("retrieve thing")
+        .get("${location}")
+        .header(HttpHeaderNames.Authorization, token)
+        .check(status.is(200)))
 
-  private val token = {
-    val res = Http(s"$UsersURL/tokens")
-      .postData(User)
-      .header(HttpHeaderNames.ContentType, ContentType)
-      .asString
-      .body
-
-    val cursor = parse(res).getOrElse(Json.Null).hcursor
-    cursor.downField("token").as[String].getOrElse("")
+    setUp(scn.inject(constantUsersPerSec(RequestsPerSecond) during 15.seconds)).protocols(httpProtocol(ThingsURL))
   }
-
-  private val httpProtocol = http
-    .baseURL(ThingsURL)
-    .inferHtmlResources()
-    .acceptHeader("*/*")
-    .contentTypeHeader(ContentType)
-    .userAgentHeader("curl/7.54.0")
-
-  private val scn = scenario("create and retrieve things")
-    .exec(http("create thing")
-      .post("/clients")
-      .header(HttpHeaderNames.ContentType, ContentType)
-      .header(HttpHeaderNames.Authorization, token)
-      .body(StringBody(Thing))
-      .check(status.is(201))
-      .check(headerRegex(HttpHeaderNames.Location, "(.*)").saveAs("location")))
-    .exec(http("retrieve thing")
-      .get("${location}")
-      .header(HttpHeaderNames.Authorization, token)
-      .check(status.is(200)))
-
-  setUp(scn.inject(constantUsersPerSec(RequestsPerSecond) during 15.seconds)).protocols(httpProtocol)
 }
 
-object CreateAndRetrieveThings {
-  val ContentType = "application/json"
-  val User = """{"email":"john.doe@email.com", "password":"123"}"""
-  val Thing = """{"type":"device", "name":"weio"}"""
-}
--- load-test/src/test/scala/com/mainflux/loadtest/PublishMessages.scala
+++ load-test/src/test/scala/com/mainflux/loadtest/PublishMessages.scala
@@ -1,88 +1,80 @@
 package com.mainflux.loadtest
 
-import com.mainflux.loadtest.Constants._
 import io.circe._
 import io.circe.parser._
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.http.protocol.HttpProtocolBuilder.toHttpProtocol
 import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
 import scalaj.http.Http
 
 import scala.concurrent.duration._
 
-final class PublishMessages extends Simulation {
-  import PublishMessages._
+final class PublishMessages extends TestCase {
+  override def prepareAndExecute(): SetUp = {
+    def makeThing(token: String): (String, String) = {
+      val thing = """{"type":"device", "name":"weio"}"""
 
-  Http(s"$UsersURL/users")
-    .postData(User)
-    .header(HttpHeaderNames.ContentType, ContentType)
-    .asString
+      val id = Http(s"$ThingsURL/clients")
+        .postData(thing)
+        .header(HttpHeaderNames.Authorization, token)
+        .header(HttpHeaderNames.ContentType, jsonType)
+        .asString
+        .headers(HttpHeaderNames.Location)(0).split("/")(2)
 
-  private val token = {
-    val res = Http(s"$UsersURL/tokens")
-      .postData(User)
-      .header(HttpHeaderNames.ContentType, ContentType)
-      .asString
-      .body
+      val res = Http(s"$ThingsURL/things/$id")
+        .header(HttpHeaderNames.Authorization, token)
+        .header(HttpHeaderNames.ContentType, jsonType)
+        .asString
+        .body
 
-    val cursor = parse(res).getOrElse(Json.Null).hcursor
-    cursor.downField("token").as[String].getOrElse("")
+      val key = parse(res).getOrElse(Json.Null).hcursor.downField("key").as[String].getOrElse("")
+
+      (id, key)
+    }
+
+    def makeChannel(token: String): String = {
+      val channel = """{"name":"mychan"}"""
+
+      Http(s"$ThingsURL/channels")
+        .postData(channel)
+        .header(HttpHeaderNames.Authorization, token)
+        .header(HttpHeaderNames.ContentType, jsonType)
+        .asString
+        .headers(HttpHeaderNames.Location)(0)
+        .split("/")(2)
+    }
+
+    def connect(channel: String, thing: String, token: String): Unit = {
+      Http(s"$ThingsURL/channels/$channel/things/$thing")
+        .method("PUT")
+        .header(HttpHeaderNames.Authorization, token)
+        .asString
+    }
+
+    val message =
+      """
+        |[
+        | {"bn":"some-base-name:","bt":1.276020076001e+09,"bu":"A","bver":5,"n":"voltage","u":"V","v":120.1},
+        | {"n":"current","t":-5,"v":1.2},
+        | {"n":"current","t":-4,"v":1.3}
+        |]""".stripMargin
+
+    val token = authenticate()
+    val (thingID, thingKey) = makeThing(token)
+    val channelID = makeChannel(token)
+
+    connect(channelID, thingID, thingKey)
+
+    val scn = scenario("publish message")
+      .exec(http("publish message request")
+        .post(s"/channels/$channelID/messages")
+        .header(HttpHeaderNames.ContentType, "application/senml+json")
+        .header(HttpHeaderNames.Authorization, thingKey)
+        .body(StringBody(message))
+        .check(status.is(202)))
+
+    setUp(scn.inject(constantUsersPerSec(RequestsPerSecond) during 15.seconds)).protocols(httpProtocol(url()))
   }
 
-  private val thingID = Http(s"$ThingsURL/clients")
-    .postData(Client)
-    .header(HttpHeaderNames.Authorization, token)
-    .header(HttpHeaderNames.ContentType, ContentType)
-    .asString
-    .headers("Location")(0).split("/")(2)
-
-  private val thingAccessKey = {
-    val res = Http(s"$ThingsURL/clients/$thingID")
-      .header(HttpHeaderNames.Authorization, token)
-      .header(HttpHeaderNames.ContentType, ContentType)
-      .asString
-      .body
-
-    val cursor = parse(res).getOrElse(Json.Null).hcursor
-    cursor.downField("key").as[String].getOrElse("")
-  }
-
-  private val chanID = Http(s"$ThingsURL/channels")
-    .postData(Channel)
-    .header(HttpHeaderNames.Authorization, token)
-    .header(HttpHeaderNames.ContentType, ContentType)
-    .asString
-    .headers("Location")(0)
-    .split("/")(2)
-
-  Http(s"$ThingsURL/channels/$chanID/things/$thingID")
-    .method("PUT")
-    .header(HttpHeaderNames.Authorization, token)
-    .asString
-
-  private val httpProtocol = http
-    .baseURL(HttpAdapterURL)
-    .inferHtmlResources()
-    .acceptHeader("*/*")
-    .contentTypeHeader(ContentType)
-    .userAgentHeader("curl/7.54.0")
-
-  private val scn = scenario("PublishMessage")
-    .exec(http("PublishMessageRequest")
-      .post(s"/channels/$chanID/messages")
-      .header(HttpHeaderNames.ContentType, "application/senml+json")
-      .header(HttpHeaderNames.Authorization, thingAccessKey)
-      .body(StringBody(Message))
-      .check(status.is(202)))
-
-  setUp(scn.inject(constantUsersPerSec(RequestsPerSecond) during 15.seconds)).protocols(httpProtocol)
-}
-
-object PublishMessages {
-  val ContentType = "application/json"
-  val User = """{"email":"john.doe@email.com", "password":"123"}"""
-  val Client = """{"type":"device", "name":"weio"}"""
-  val Channel = """{"name":"mychan"}"""
-  val Message = """[{"bn":"some-base-name:","bt":1.276020076001e+09, "bu":"A","bver":5, "n":"voltage","u":"V","v":120.1}, {"n":"current","t":-5,"v":1.2}, {"n":"current","t":-4,"v":1.3}]"""
+  private def url(): String = System.getProperty("http", "http://localhost:8185")
 }
