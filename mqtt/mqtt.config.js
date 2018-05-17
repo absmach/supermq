66f286e677f294f9c812a8863dd7e13dda68c0ab
Aleksandar Novakovic <aleksandar.novakovic@mainflux.com>
2018-05-17 10:32:21 +0200

    Remove WebSocket env variable from MQTT adapter
    Signed-off-by: Aleksandar Novakovic <aleksandar.novakovic@mainflux.com>

--- mqtt/mqtt.config.js
+++ mqtt/mqtt.config.js
@@ -3,7 +3,6 @@
 // Service configuration
 module.exports = {
     mqtt_port: process.env.MF_MQTT_ADAPTER_PORT || 1883,
-    ws_port: process.env.WS_PORT || 8880,
     // NATS broker URL
     nats_url: process.env.MF_NATS_URL || 'nats://localhost:4222',
     // Auth service URL
