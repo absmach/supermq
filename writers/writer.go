a3bd5d1f62b483e122bb17b6c51c90a93b657532
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2018-05-21 15:54:05 +0200

    Move consume method at the end of file
    Signed-off-by: Dušan Borovčanin <dusan.borovcanin@mainflux.com>

--- writers/writer.go
+++ writers/writer.go
@@ -19,20 +19,6 @@ type consumer struct {
 	repo   MessageRepository
 }
 
-func (c *consumer) consume(m *nats.Msg) {
-	msg := &mainflux.Message{}
-
-	if err := proto.Unmarshal(m.Data, msg); err != nil {
-		c.logger.Warn(fmt.Sprintf("%s failed to unmarshal received message: %s", c.name, err))
-		return
-	}
-
-	if err := c.repo.Save(*msg); err != nil {
-		c.logger.Warn(fmt.Sprintf("%s failed to save message: %s", c.name, err))
-		return
-	}
-}
-
 // Start method starts to consume normalized messages received from NATS.
 func Start(name string, nc *nats.Conn, logger log.Logger, repo MessageRepository, counter metrics.Counter, latency metrics.Histogram) error {
 	repo = newMetricsMiddleware(repo, counter, latency)
@@ -45,4 +31,18 @@ func Start(name string, nc *nats.Conn, logger log.Logger, repo MessageRepository
 
 	_, err := nc.Subscribe(senML, consumer.consume)
 	return err
+}
+
+func (c *consumer) consume(m *nats.Msg) {
+	msg := &mainflux.Message{}
+
+	if err := proto.Unmarshal(m.Data, msg); err != nil {
+		c.logger.Warn(fmt.Sprintf("%s failed to unmarshal received message: %s", c.name, err))
+		return
+	}
+
+	if err := c.repo.Save(*msg); err != nil {
+		c.logger.Warn(fmt.Sprintf("%s failed to save message: %s", c.name, err))
+		return
+	}
 }
