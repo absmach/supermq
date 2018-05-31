b1c0fe145862188537a52c4c6614e64dfe00c88b
Manuel Imperiale <manuel.imperiale@gmail.com>
2018-05-31 19:39:27 +0200

    Set certs paths via env variables
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- bashflux/http.go
+++ bashflux/http.go
@@ -6,18 +6,28 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+
+	"github.com/manuio/mainflux"
+)
+
+const (
+	envCertFile = "MF_CERT_FILE"
+	envKeyFile  = "MF_KEY_FILE"
+	envCaFile   = "MF_CA_FILE"
 )
 
 var (
 	httpClient = &http.Client{}
 	serverAddr = "https://0.0.0.0"
-)
 
-const (
-	certFile = "../../docker/ssl/certs/mainflux-server.crt"
-	keyFile  = "../../docker/ssl/certs/mainflux-server.key"
-	caFile   = "../../docker/ssl/certs/ca.crt"
+	certFile = os.Getenv("GOPATH") +
+		"src/github.com/mainflux/mainflux/docker/ssl/certs/mainflux-server.crt"
+	keyFile = os.Getenv("GOPATH") +
+		"src/github.com/mainflux/mainflux/docker/ssl/certs/mainflux-server.key"
+	caFile = os.Getenv("GOPATH") +
+		"src/github.com/mainflux/mainflux/docker/ssl/certs/ca.crt"
 )
 
 // SetServerAddr - set addr using host and port
@@ -30,6 +40,11 @@ func SetServerAddr(host string, port int) {
 }
 
 func SetCerts() {
+	// Set certificates paths
+	certFile = mainflux.Env(envCertFile, certFile)
+	keyFile = mainflux.Env(envKeyFile, keyFile)
+	caFile = mainflux.Env(envCaFile, caFile)
+
 	// Load client cert
 	cert, err := tls.LoadX509KeyPair(certFile, keyFile)
 	if err != nil {
