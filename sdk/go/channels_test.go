177818b7e4a23ee222455e1dd04d95331a21d369
Ivan Milošević <iva@blokovi.com>
2018-11-01 09:49:51 +0100

    Add SDK tests for Channel function
    Signed-off-by: Ivan Milošević <iva@blokovi.com>

--- sdk/go/channels_test.go
+++ sdk/go/channels_test.go
@@ -10,6 +10,7 @@ package sdk_test
 import (
 	"fmt"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 
 	"github.com/mainflux/mainflux/sdk/go"
@@ -105,5 +106,105 @@ func TestCreateChannel(t *testing.T) {
 		assert.Equal(t, tc.location, loc, fmt.Sprintf("%s: expected location %s got %s", tc.desc, tc.location, loc))
 
 	}
+}
+func TestChannel(t *testing.T) {
+	svc := newService(map[string]string{token: email})
+	ts := newServer(svc)
+	defer ts.Close()
+	sdkConf := sdk.Config{
+		BaseURL:           ts.URL,
+		UsersPrefix:       "",
+		ThingsPrefix:      "",
+		HTTPAdapterPrefix: "http",
+		MsgContentType:    contentType,
+		TLSVerification:   false,
+	}
 
+	mainfluxSDK := sdk.NewSDK(sdkConf)
+	channel := sdk.Channel{ID: "1", Name: "test"}
+	mainfluxSDK.CreateChannel(channel, token)
+
+	cases := []struct {
+		desc     string
+		chId     string
+		token    string
+		err      error
+		response sdk.Channel
+	}{
+		{
+			desc:     "Get existing channel",
+			chId:     "1",
+			token:    token,
+			err:      nil,
+			response: channel,
+		},
+		{
+			desc:     "Get non-existent channel",
+			chId:     "43",
+			token:    token,
+			err:      sdk.ErrNotFound,
+			response: sdk.Channel{},
+		},
+		{
+			desc:     "Get channel with invalid token",
+			chId:     "1",
+			token:    wrongValue,
+			err:      sdk.ErrUnauthorized,
+			response: sdk.Channel{},
+		},
+	}
+
+	for _, tc := range cases {
+		respCh, err := mainfluxSDK.Channel(tc.chId, tc.token)
+
+		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: unexpected error %s", tc.desc, err))
+		assert.Equal(t, tc.response, respCh, fmt.Sprintf("%s: expected response channel %s, got %s", tc.desc, tc.response, respCh))
+	}
+
+}
+
+func TestCahnnels(t *testing.T) {
+
+	svc := newService(map[string]string{token: email})
+	ts := newServer(svc)
+	defer ts.Close()
+	sdkConf := sdk.Config{
+		BaseURL:           ts.URL,
+		UsersPrefix:       "",
+		ThingsPrefix:      "",
+		HTTPAdapterPrefix: "http",
+		MsgContentType:    contentType,
+		TLSVerification:   false,
+	}
+	var channels []sdk.Channel
+
+	mainfluxSDK := sdk.NewSDK(sdkConf)
+	for i := 1; i < 101; i++ {
+
+		channel := sdk.Channel{ID: strconv.Itoa(i), Name: "test"}
+		mainfluxSDK.CreateChannel(channel, token)
+		channels = append(channels, channel)
+	}
+
+	cases := []struct {
+		desc     string
+		chId     string
+		token    string
+		err      error
+		response []sdk.Channel
+	}{
+		{
+			desc:     "get a list of channels",
+			chId:     "1",
+			token:    token,
+			err:      nil,
+			response: channels[0:5],
+		},
+	}
+	for _, tc := range cases {
+		respChs, err := mainfluxSDK.Channels(tc.token, 0, 5)
+		assert.Equal(t, tc.err, err, fmt.Sprintf("%s: unexpected error %s", tc.desc, err))
+		assert.Equal(t, tc.response, respChs, fmt.Sprintf("%s: expected response channel %s, got %s", tc.desc, tc.response, respChs))
+
+	}
 }
