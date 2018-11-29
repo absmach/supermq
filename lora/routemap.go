60ffc9a6d1695644872b5c5e58d47023b822f386
Manuel Imperiale <manuel.imperiale@gmail.com>
2018-11-29 10:06:43 +0100

    Add routemap and handle event sourcing
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- lora/routemap.go
+++ lora/routemap.go
@@ -10,11 +10,11 @@ package lora
 // RouteMapRepository store route map between Lora App Server and Mainflux
 type RouteMapRepository interface {
 	// Save stores/routes pair lora application topic & mainflux channel.
-	Save(string, uint64) error
+	Save(string, string) error
 
 	// Channel returns mainflux channel for given lora application.
-	Channel(string) (uint64, error)
+	Get(string) (string, error)
 
 	// Removes mapping from cache.
-	Remove(uint64) error
+	Remove(string) error
 }
