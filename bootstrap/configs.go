1b0ae76cc62d16efdc6c2aae80e499918cc02cb0
Dusan Borovcanin <dusan.borovcanin@mainflux.com>
2019-01-09 09:01:12 +0100

    Move State to separate file
    Signed-off-by: Dusan Borovcanin <dusan.borovcanin@mainflux.com>

--- bootstrap/configs.go
+++ bootstrap/configs.go
@@ -7,25 +7,8 @@
 
 package bootstrap
 
-import (
-	"strconv"
-)
-
-// State represents corresponding Mainflux Thing state. The possible Config States
-// as well as description of what that State represents are given in the table:
-// | State    | What it means 		                                                           |
-// |----------+--------------------------------------------------------------------------------|
-// | Inactive | Thing is created, but isn't able to communicate over Mainflux 				   |
-// | Active   | Thing is able to communicate using Mainflux                                    |
-type State int
-
-// String returns string representation of State.
-func (s State) String() string {
-	return strconv.Itoa(int(s))
-}
-
 const (
-	// Inactive Thing is created and configured, but not able to exchange messages using Mainflux.
+	// Inactive Thing is created, but not able to exchange messages using Mainflux.
 	Inactive State = iota
 	// Active Thing is created, configured, and whitelisted.
 	Active
