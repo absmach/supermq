5a6fba6bc9984ba54c4fa4abf16aa51b5857bcd6
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2019-03-29 20:22:12 +0100

    Add message type check before message parsing
    Signed-off-by: Dušan Borovčanin <dusan.borovcanin@mainflux.com>

--- docker/ssl/authorization.js
+++ docker/ssl/authorization.js
@@ -1,5 +1,6 @@
 var clientKey = '';
 
+// Check certificate MQTTS.
 function authenticate(s) {
     if (!s.variables.ssl_client_s_dn || !s.variables.ssl_client_s_dn.length) {
         s.deny();
@@ -9,6 +10,16 @@ function authenticate(s) {
     s.on('upload', function (data) {
         while (data == '') {
             return s.AGAIN
+        }
+
+        var packet_type_flags_byte = data.codePointAt(0);
+        // First MQTT packet contain message type and flags. CONNECTON message type
+        // is encoded as 0001, and we're not interested in flags, so only values
+        // 0001xxxx (which is between 16 and 32) should be checked.
+        if (packet_type_flags_byte < 16 || packet_type_flags_byte >= 32) {
+            s.off('upload');
+            s.allow();
+            return;
         }
 
         if (clientKey === '') {
@@ -21,6 +32,7 @@ function authenticate(s) {
             s.error('Cert CN (' + clientKey + ') does not match client ID');
             s.off('upload')
             s.deny();
+            return;
         }
 
         s.off('upload');
@@ -37,16 +49,16 @@ function parsePackage(s, data) {
 
     /*
         0               1               2               3
-	    7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0
-	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	    | TYPE | RSRVD | REMAINING LEN |      PROTOCOL NAME LEN       |
-	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        | TYPE | RSRVD | REMAINING LEN |      PROTOCOL NAME LEN       |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 	    |                        PROTOCOL NAME                        |    
-	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-|
-	    |    VERSION   |     FLAGS     |          KEEP ALIVE          | 
         +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-|
-	    |                     Payload (if any) ...                    |
-	    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        |    VERSION   |     FLAGS     |          KEEP ALIVE          | 
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-|
+        |                     Payload (if any) ...                    |
+        +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
        
         First byte with remaining length represents fixed header.
         Remaining Length is the length of the variable header (10 bytes) plus the length of the Payload.
@@ -54,8 +66,8 @@ function parsePackage(s, data) {
         http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/errata01/os/mqtt-v3.1.1-errata01-os-complete.html#_Toc442180836.
         
         Connect flags byte looks like this:   
-        |       7       |       6       |       5     |   4  3   |     2     |     1        |     0     |
-        | Username Flag | Password Flag | Will Retain | Will QoS | Will Flag | Clean Session | Reserved |
+        |       7       |       6       |       5     |   4  3   |     2     |       1       |     0     |
+        | Username Flag | Password Flag | Will Retain | Will QoS | Will Flag | Clean Session | Reserved  |
 
         The payload is determined by the flags and comes in this order:
             1. Client ID (2 bytes length + ID value)
@@ -67,56 +79,48 @@ function parsePackage(s, data) {
         This method extracts Password field.
 	*/
 
-    var packet_type_flags_byte = data.codePointAt(0);
-    // First MQTT packet contain message type and flags. CONN message type
-    // is encoded as 0001, and we're not interested in flags, so all values
-    // 0001xxxx are valid for us, which is between 16 and 32.
-    if (packet_type_flags_byte >= 16 && packet_type_flags_byte < 32) {
-        // Extract variable length header. It's 1-4 bytes. As long as continuation byte is
-        // 1, there are more bytes in this header. This algorithm is explained here:
-        // http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/errata01/os/mqtt-v3.1.1-errata01-os-complete.html#_Toc442180836
-        var len_size = 1;
-        for (var remaining_len = 1; remaining_len < 5; remaining_len++) {
-            if (data.codePointAt(remaining_len) > 128) {
-                len_size += 1;
-                continue;
-            }
-            break;
+    // Extract variable length header. It's 1-4 bytes. As long as continuation byte is
+    // 1, there are more bytes in this header. This algorithm is explained here:
+    // http://docs.oasis-open.org/mqtt/mqtt/v3.1.1/errata01/os/mqtt-v3.1.1-errata01-os-complete.html#_Toc442180836
+    var len_size = 1;
+    for (var remaining_len = 1; remaining_len < 5; remaining_len++) {
+        if (data.codePointAt(remaining_len) > 128) {
+            len_size += 1;
+            continue;
         }
-        // CONTROL(1) + MSG_LEN(1-4) + PROTO_NAME_LEN(2) + PROTO_NAME(4) + PROTO_VERSION(1)
-        var flags_pos = 1 + len_size + 2 + 4 + 1;
-        var flags = data.codePointAt(flags_pos);
-        // If there are no username and password flags (11xxxxxx), return.
-        if (flags < 192) {
-            s.error('MQTT username or password not provided');
-            return '';
-        }
-        // FLAGS(1) + KEEP_ALIVE(2)
-        var shift = flags_pos + 1 + 2;
+        break;
+    }
+    // CONTROL(1) + MSG_LEN(1-4) + PROTO_NAME_LEN(2) + PROTO_NAME(4) + PROTO_VERSION(1)
+    var flags_pos = 1 + len_size + 2 + 4 + 1;
+    var flags = data.codePointAt(flags_pos);
+    // If there are no username and password flags (11xxxxxx), return.
+    if (flags < 192) {
+        s.error('MQTT username or password not provided');
+        return '';
+    }
+    // FLAGS(1) + KEEP_ALIVE(2)
+    var shift = flags_pos + 1 + 2;
 
-        // Number of bytes to encode length.
-        var len_bytes_num = 2;
-        // If Wil Flag is present, Will Topic and Will Message need to be skipped as well.
-        var shift_flags = 196 <= flags ? 5 : 3;
-        var len_msb, len_lsb, len;
-        for (var i = 0; i < shift_flags; i++) {
-            len_msb = data.codePointAt(shift).toString(16);
-            len_lsb = data.codePointAt(shift + 1).toString(16);
-            len = calcLen(len_msb, len_lsb);
-            shift += len_bytes_num;
-            if (i != shift_flags - 1) {
-                shift += len;
-            }
+    // Number of bytes to encode length.
+    var len_bytes_num = 2;
+    // If Wil Flag is present, Will Topic and Will Message need to be skipped as well.
+    var shift_flags = 196 <= flags ? 5 : 3;
+    var len_msb, len_lsb, len;
+    for (var i = 0; i < shift_flags; i++) {
+        len_msb = data.codePointAt(shift).toString(16);
+        len_lsb = data.codePointAt(shift + 1).toString(16);
+        len = calcLen(len_msb, len_lsb);
+        shift += len_bytes_num;
+        if (i != shift_flags - 1) {
+            shift += len;
         }
-
-        var password = data.substring(shift, shift + len);
-        return password;
     }
 
-    return '';
-
+    var password = data.substring(shift, shift + len);
+    return password;
 }
 
+// Check certificate HTTPS and WSS.
 function setKey(r) {
     if (clientKey === '') {
         clientKey = parseCert(r.variables.ssl_client_s_dn, 'CN');
