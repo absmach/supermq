4d3e8787f074cb78062a2ee57d64edd1fe2c4317
Ivan Milošević <iva@blokovi.com>
2019-06-11 12:11:14 +0200

    update apt
    Signed-off-by: Ivan Milošević <iva@blokovi.com>

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -67,6 +67,7 @@ run_test() {
 
 install_qemu() {
 	echo "Installing qemu..."
+	sudo apt update
 	sudo apt -y install qemu qemu-user-static qemu-user binfmt-support
 	sudo mv /usr/bin/qemu-arm-static $MF_PATH
 }
