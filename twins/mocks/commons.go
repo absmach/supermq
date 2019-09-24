d42e46ab58f4a36288efaae5ad2a38f10bddb2cc
Darko Draskovic <darko.draskovic@gmail.com>
2019-09-24 19:52:59 +0200

    Add twin repo mock
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- twins/mocks/commons.go
+++ twins/mocks/commons.go
@@ -11,7 +11,8 @@ import "fmt"
 
 // Since mocks will store data in map, and they need to resemble the real
 // identifiers as much as possible, a key will be created as combination of
-// owner and their id. This will allow searching by prefix or suffix.
+// owner and their own identifiers. This will allow searching either by
+// prefix or suffix.
 func key(owner string, id string) string {
 	return fmt.Sprintf("%s-%s", owner, id)
 }
--- twins/mocks/twins.go
+++ twins/mocks/twins.go
@@ -9,10 +9,7 @@ package mocks
 
 import (
 	"context"
-	"fmt"
-	"sort"
 	"strconv"
-	"strings"
 	"sync"
 
 	"github.com/mainflux/mainflux/twins"
@@ -24,30 +21,28 @@ type twinRepositoryMock struct {
 	mu      sync.Mutex
 	counter uint64
 	twins   map[string]twins.Twin
-	tconns  map[string]map[string]twins.Twin
 }
 
 // NewTwinRepository creates in-memory twin repository.
 func NewTwinRepository() twins.TwinRepository {
 	return &twinRepositoryMock{
-		twins:  make(map[string]twins.Twin),
-		tconns: make(map[string]map[string]twins.Twin),
+		twins: make(map[string]twins.Twin),
 	}
 }
 
-func (trm *twinRepositoryMock) Save(ctx context.Context, twin twins.Twin) (string, error) {
+func (trm *twinRepositoryMock) Save(ctx context.Context, twin twins.Twin) error {
 	trm.mu.Lock()
 	defer trm.mu.Unlock()
 
 	for _, tw := range trm.twins {
 		if tw.ID == twin.ID {
-			return "", twins.ErrConflict
+			return twins.ErrConflict
 		}
 	}
 
 	for _, tw := range trm.twins {
 		if tw.Key == twin.Key {
-			return "", twins.ErrConflict
+			return twins.ErrConflict
 		}
 	}
 
@@ -55,7 +50,7 @@ func (trm *twinRepositoryMock) Save(ctx context.Context, twin twins.Twin) (strin
 	twin.ID = strconv.FormatUint(trm.counter, 10)
 	trm.twins[key(twin.Owner, twin.ID)] = twin
 
-	return twin.ID, nil
+	return nil
 }
 
 func (trm *twinRepositoryMock) Update(ctx context.Context, twin twins.Twin) error {
@@ -73,132 +68,38 @@ func (trm *twinRepositoryMock) Update(ctx context.Context, twin twins.Twin) erro
 	return nil
 }
 
-func (trm *twinRepositoryMock) UpdateKey(ctx context.Context, owner, id, val string) error {
+func (trm *twinRepositoryMock) UpdateKey(ctx context.Context, id, key string) error {
 	trm.mu.Lock()
 	defer trm.mu.Unlock()
 
 	for _, tw := range trm.twins {
-		if tw.Key == val {
+		if tw.Key == key {
 			return twins.ErrConflict
 		}
 	}
 
-	dbKey := key(owner, id)
-
-	tw, ok := trm.twins[dbKey]
-	if !ok {
-		return twins.ErrNotFound
-	}
-
-	tw.Key = val
-	trm.twins[dbKey] = tw
+	// dbKey := key()
 
 	return nil
 }
 
-func (trm *twinRepositoryMock) RetrieveByID(_ context.Context, owner, id string) (twins.Twin, error) {
+func (trm *twinRepositoryMock) RetrieveByID(_ context.Context, id string) (twins.Twin, error) {
 	trm.mu.Lock()
 	defer trm.mu.Unlock()
 
-	if tw, ok := trm.twins[key(owner, id)]; ok {
-		return tw, nil
-	}
-
-	return twins.Twin{}, twins.ErrNotFound
+	return twins.Twin{}, nil
 }
 
-func (trm *twinRepositoryMock) RetrieveByKey(_ context.Context, key string) (string, error) {
+func (trm *twinRepositoryMock) RetrieveByKey(_ context.Context, key string) (twins.Twin, error) {
 	trm.mu.Lock()
 	defer trm.mu.Unlock()
 
-	for _, twin := range trm.twins {
-		if twin.Key == key {
-			return twin.ID, nil
-		}
-	}
-
-	return "", twins.ErrNotFound
+	return twins.Twin{}, nil
 }
 
-func (trm *twinRepositoryMock) RetrieveAll(_ context.Context, owner string, limit uint64, name string, metadata twins.Metadata) (twins.TwinsSet, error) {
+func (trm *twinRepositoryMock) Remove(ctx context.Context, id string) error {
 	trm.mu.Lock()
 	defer trm.mu.Unlock()
-
-	items := make([]twins.Twin, 0)
-
-	if limit <= 0 {
-		return twins.TwinsSet{}, nil
-	}
-
-	last := uint64(limit)
-
-	// This obscure way to examine map keys is enforced by the key structure
-	// itself (see mocks/commons.go).
-	prefix := fmt.Sprintf("%s-", owner)
-	for k, v := range trm.twins {
-		id, _ := strconv.ParseUint(v.ID, 10, 64)
-		if strings.HasPrefix(k, prefix) && id < last {
-			items = append(items, v)
-		}
-	}
-
-	sort.SliceStable(items, func(i, j int) bool {
-		return items[i].ID < items[j].ID
-	})
-
-	page := twins.TwinsSet{
-		Twins: items,
-		SetMetadata: twins.SetMetadata{
-			Total: trm.counter,
-			Limit: limit,
-		},
-	}
-
-	return page, nil
-}
-
-func (trm *twinRepositoryMock) RetrieveByChannel(_ context.Context, chanID string, limit uint64) (twins.TwinsSet, error) {
-	trm.mu.Lock()
-	defer trm.mu.Unlock()
-
-	items := make([]twins.Twin, 0)
-
-	if limit <= 0 {
-		return twins.TwinsSet{}, nil
-	}
-
-	tws, ok := trm.tconns[chanID]
-	if !ok {
-		return twins.TwinsSet{}, nil
-	}
-
-	for _, v := range tws {
-		id, _ := strconv.ParseUint(v.ID, 10, 64)
-		if id < limit {
-			items = append(items, v)
-		}
-	}
-
-	sort.SliceStable(items, func(i, j int) bool {
-		return items[i].ID < items[j].ID
-	})
-
-	page := twins.TwinsSet{
-		Twins: items,
-		SetMetadata: twins.SetMetadata{
-			Total: trm.counter,
-			Limit: limit,
-		},
-	}
-
-	return page, nil
-}
-
-func (trm *twinRepositoryMock) Remove(ctx context.Context, owner, id string) error {
-	trm.mu.Lock()
-	defer trm.mu.Unlock()
-
-	delete(trm.twins, key(owner, id))
 
 	return nil
 }
