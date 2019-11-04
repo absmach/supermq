d1a4d1384408d242660bb01fbd4c2ce7e8eb69c6
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2019-11-04 15:34:46 +0100

    Remove remaining RawMessage reference
    Signed-off-by: Dušan Borovčanin <dusan.borovcanin@mainflux.com>

--- topics.go
+++ topics.go
@@ -6,5 +6,5 @@ package mainflux
 // File topics.go contains all NATS subjects that are shared between services.
 
 // InputChannels represents subject all messages will be published to.
-// Messages received on this topic are Protobuf serialized RawMessages.
+// Messages received on this topic are Protobuf serialized Messages.
 const InputChannels = "channel.>"
