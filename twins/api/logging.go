9e5bf9a79c0d196b5645433fa685a6e2956d4ce0
Darko Draskovic <darko.draskovic@gmail.com>
2019-11-13 20:37:03 +0100

    Add RetrieveAll to twinRepository and ListTwins to service
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- twins/api/logging.go
+++ twins/api/logging.go
@@ -95,6 +95,19 @@ func (lm *loggingMiddleware) ViewTwin(ctx context.Context, token, id string) (vi
 	return lm.svc.ViewTwin(ctx, token, id)
 }
 
+func (lm *loggingMiddleware) ListTwins(ctx context.Context, token string, limit uint64, name string, metadata twins.SetMetadata) (tw twins.TwinsSet, err error) {
+	defer func(begin time.Time) {
+		message := fmt.Sprintf("Method list_twins for for token %s took %s to complete", token, time.Since(begin))
+		if err != nil {
+			lm.logger.Warn(fmt.Sprintf("%s with error: %s.", message, err))
+			return
+		}
+		lm.logger.Info(fmt.Sprintf("%s without errors.", message))
+	}(time.Now())
+
+	return lm.svc.ListTwins(ctx, token, limit, name, metadata)
+}
+
 func (lm *loggingMiddleware) ListTwinsByChannel(ctx context.Context, token, channel string, limit uint64) (tw twins.TwinsSet, err error) {
 	defer func(begin time.Time) {
 		message := fmt.Sprintf("Method list_twins_by_channel for for token %s and channel %s took %s to complete", token, channel, time.Since(begin))
