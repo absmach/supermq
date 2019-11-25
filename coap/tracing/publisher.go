9bafe494bb69bad7effe8785b3f3bad3b355e226
Nick Neisen <nwneisen@gmail.com>
2019-11-25 16:40:19 -0700

    Add NATS tracing to COAP
    Signed-off-by: Nick Neisen <nwneisen@gmail.com>

---
+++ coap/tracing/publisher.go
@@ -0,0 +1,54 @@
+package tracing
+
+import (
+	"context"
+
+	"github.com/mainflux/mainflux"
+	"github.com/mainflux/mainflux/coap"
+	"github.com/opentracing/opentracing-go"
+)
+
+const (
+	publishOP   = "publish_op"
+	subscribeOP = "subscribe_op"
+)
+
+var _ mainflux.MessagePublisher = (*natsPublisherMiddleware)(nil)
+
+type natsPublisherMiddleware struct {
+	tracer opentracing.Tracer
+	pubsub coap.Broker
+}
+
+// NatsPublisherMiddleware add spans to context
+func NatsPublisherMiddleware(pubsub coap.Broker, tracer opentracing.Tracer) coap.Broker {
+	return natsPublisherMiddleware{
+		tracer: tracer,
+		pubsub: pubsub,
+	}
+}
+
+func (npm natsPublisherMiddleware) Publish(ctx context.Context, token string, msg mainflux.Message) error {
+	span := createSpan(ctx, npm.tracer, publishOP)
+	defer span.Finish()
+	npm.tracer.Inject(span.Context(), opentracing.Binary, &msg.Span)
+
+	return npm.pubsub.Publish(ctx, token, msg)
+}
+
+func (npm natsPublisherMiddleware) Subscribe(chanID, subtopic, obsID string, observer *coap.Observer) error {
+	span := createSpan(context.TODO(), npm.tracer, subscribeOP)
+	defer span.Finish()
+
+	return npm.Subscribe(chanID, subtopic, obsID, observer)
+}
+
+func createSpan(ctx context.Context, tracer opentracing.Tracer, opName string) opentracing.Span {
+	if parentSpan := opentracing.SpanFromContext(ctx); parentSpan != nil {
+		return tracer.StartSpan(
+			opName,
+			opentracing.ChildOf(parentSpan.Context()),
+		)
+	}
+	return tracer.StartSpan(opName)
+}
