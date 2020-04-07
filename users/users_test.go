e79b16b1278bee013e1d73355893c95f98910eb6
Manuel Imperiale <manuel.imperiale@gmail.com>
2020-04-07 09:40:51 +0200

    Revert users test
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- users/users_test.go
+++ users/users_test.go
@@ -147,6 +147,6 @@ func TestValidate(t *testing.T) {
 
 	for desc, tc := range cases {
 		err := tc.user.Validate()
-		assert.True(t, errors.Contains(tc.err, err), fmt.Sprintf("%s: expected %s got %s\n", desc, tc.err, err))
+		assert.True(t, errors.Contains(err, tc.err), fmt.Sprintf("%s: expected %s got %s\n", desc, tc.err, err))
 	}
 }
