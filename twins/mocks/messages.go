1c50f8c6f8cdd4cda45899928d1f213c3040122a
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2020-04-27 20:45:30 +0200

    Fix Twins mocks
    Signed-off-by: Dušan Borovčanin <dusan.borovcanin@mainflux.com>

--- twins/mocks/messages.go
+++ twins/mocks/messages.go
@@ -4,24 +4,24 @@
 package mocks
 
 import (
-	"github.com/mainflux/mainflux"
 	"github.com/mainflux/mainflux/errors"
+	"github.com/mainflux/mainflux/messaging"
 )
 
-var _ mainflux.Publisher = (*mockBroker)(nil)
+var _ messaging.Publisher = (*mockBroker)(nil)
 
 type mockBroker struct {
 	subscriptions map[string]string
 }
 
 // New returns mock message publisher.
-func New(sub map[string]string) mainflux.Publisher {
+func New(sub map[string]string) messaging.Publisher {
 	return &mockBroker{
 		subscriptions: sub,
 	}
 }
 
-func (mb mockBroker) Publish(topic string, msg mainflux.Message) error {
+func (mb mockBroker) Publish(topic string, msg messaging.Message) error {
 	if len(msg.Payload) == 0 {
 		return errors.New("failed to publish")
 	}
