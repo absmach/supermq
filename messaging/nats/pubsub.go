8898cae38bf088fb0003679d315b677bbba02368
Manuel Imperiale <manuel.imperiale@gmail.com>
2020-04-30 16:03:12 +0200

    revert
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- messaging/nats/pubsub.go
+++ messaging/nats/pubsub.go
@@ -64,13 +64,13 @@ func NewPubSub(url, queue string, logger log.Logger) (PubSub, error) {
 	return ret, nil
 }
 
-func (ps *pubsub) Publish(subject string, msg messaging.Message) error {
+func (ps *pubsub) Publish(topic string, msg messaging.Message) error {
 	data, err := proto.Marshal(&msg)
 	if err != nil {
 		return err
 	}
 
-	subject = fmt.Sprintf("%s.%s", chansPrefix, subject)
+	subject := fmt.Sprintf("%s.%s", chansPrefix, topic)
 	if msg.Subtopic != "" {
 		subject = fmt.Sprintf("%s.%s", subject, msg.Subtopic)
 	}
@@ -114,6 +114,8 @@ func (ps *pubsub) Unsubscribe(topic string) error {
 	}
 	ps.mu.Lock()
 	defer ps.mu.Unlock()
+
+	topic = fmt.Sprintf("%s.%s", chansPrefix, topic)
 
 	sub, ok := ps.subscriptions[topic]
 	if !ok {
