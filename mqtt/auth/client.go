c9e0b79f649dff00b4c68e983614dcfdf00962b8
dusanb94 <dusan.borovcanin@mainflux.com>
2020-06-01 17:29:50 +0200

    Remove unused code
    Signed-off-by: dusanb94 <dusan.borovcanin@mainflux.com>

--- mqtt/auth/client.go
+++ mqtt/auth/client.go
@@ -5,7 +5,6 @@ package auth
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/go-redis/redis"
 	"github.com/mainflux/mainflux"
@@ -37,28 +36,23 @@ func New(redisClient *redis.Client, thingsClient mainflux.ThingsServiceClient) C
 
 func (c client) Identify(thingKey string) (string, error) {
 	tkey := keyPrefix + ":" + thingKey
-	fmt.Println("reading from cache identify")
 	thingID, err := c.redisClient.Get(tkey).Result()
 	if err != nil {
 		t := &mainflux.Token{
 			Value: string(thingKey),
 		}
 
-		fmt.Println("Not in cache, using gRPC identify")
 		thid, err := c.thingsClient.Identify(context.TODO(), t)
 		if err != nil {
 			return "", err
 		}
 		return thid.GetValue(), nil
 	}
-	fmt.Println("in cache, all good")
 	return thingID, nil
 }
 
 func (c client) Authorize(chanID, thingID string) error {
-	fmt.Println("readig cache authorize")
 	if c.redisClient.SIsMember(chanPrefix+":"+chanID, thingID).Val() {
-		fmt.Println("in cache")
 		return nil
 	}
 
@@ -66,7 +60,6 @@ func (c client) Authorize(chanID, thingID string) error {
 		ThingID: thingID,
 		ChanID:  chanID,
 	}
-	fmt.Println("gPRC authorize")
 	_, err := c.thingsClient.CanAccessByID(context.TODO(), ar)
 	return err
 }
