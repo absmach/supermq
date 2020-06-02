eef764c861046a4bdde12936c1f7313d0a629253
dusanb94 <dusan.borovcanin@mainflux.com>
2020-06-02 15:25:23 +0200

    Add copyrights header
    Signed-off-by: dusanb94 <dusan.borovcanin@mainflux.com>

--- pkg/doc.go
+++ pkg/doc.go
@@ -1,1 +1,6 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+// Package pkg contains library packages used by Mainflux services
+// and external services that integrate with Mainflux.
 package pkg
