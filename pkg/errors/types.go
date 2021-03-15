3e662944ea3c73a79007bdfa65002efc26567b4c
Manuel Imperiale <manuel.imperiale@gmail.com>
2021-03-15 19:00:04 +0100

    Add blank lines after error definitions
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- pkg/errors/types.go
+++ pkg/errors/types.go
@@ -6,10 +6,13 @@ package errors
 var (
 	// ErrUnsupportedContentType indicates unacceptable or lack of Content-Type
 	ErrUnsupportedContentType = New("unsupported content type")
+
 	// ErrInvalidQueryParams indicates invalid query parameters
 	ErrInvalidQueryParams = New("invalid query parameters")
+
 	// ErrNotInQuery indicates boolean parameter missing in the query
 	ErrNotInQuery = New("missing in the query")
+
 	// ErrMalformedEntity indicates a malformed entity specification
 	ErrMalformedEntity = New("malformed entity specification")
 )
