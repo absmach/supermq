f30ac6a07b65522a17470ccffe7949a9bf3c97f6
Manuel Imperiale <manuel.imperiale@gmail.com>
2021-04-09 12:24:29 +0200

    NOISSUE - Add tests and connection route-map to lora-adapter
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

---
+++ lora/mocks/routes.go
@@ -0,0 +1,57 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+package mocks
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/mainflux/mainflux/lora"
+)
+
+type routeMapMock struct {
+	mu     sync.Mutex
+	routes map[string]string
+}
+
+// NewRouteMap returns mock route-map instance.
+func NewRouteMap() lora.RouteMapRepository {
+	return &routeMapMock{
+		routes: make(map[string]string),
+	}
+}
+
+func (trm *routeMapMock) Save(mfxID, extID string) error {
+	trm.mu.Lock()
+	defer trm.mu.Unlock()
+
+	trm.routes[extID] = mfxID
+	return nil
+}
+
+func (trm *routeMapMock) Get(extID string) (string, error) {
+	trm.mu.Lock()
+	defer trm.mu.Unlock()
+
+	id, ok := trm.routes[extID]
+	if !ok {
+		return "", errors.New("route-map not found")
+	}
+
+	return id, nil
+}
+
+func (trm *routeMapMock) Remove(extID string) error {
+	trm.mu.Lock()
+	defer trm.mu.Unlock()
+
+	for i, val := range trm.routes {
+		if val == extID {
+			delete(trm.routes, i)
+			return nil
+		}
+	}
+
+	return nil
+}
