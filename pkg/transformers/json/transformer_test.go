4217d2835ad73ae31fee3e5a1fe8d2aa6dc49395
Burak Sekili <buraksekili@gmail.com>
2021-07-03 12:16:58 +0300

    Separate TestTransformJSON into two tests
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- pkg/transformers/json/transformer_test.go
+++ pkg/transformers/json/transformer_test.go
@@ -21,10 +21,9 @@ const (
 	invalidFlatPayload = `{"key1"}`
 )
 
-func TestTransformJSON(t *testing.T) {
+func TestTransformerFlat(t *testing.T) {
 	now := time.Now().Unix()
-	trFlatten := json.New(true)
-	trNoFlatten := json.New(false)
+	tr := json.New(true)
 
 	msg := messaging.Message{
 		Channel:   "channel-1",
@@ -40,22 +39,22 @@ func TestTransformJSON(t *testing.T) {
 	listMsg := msg
 	listMsg.Payload = []byte(listPayload)
 
-	flatJSONMsg := json.Message{
-		Channel:   msg.Channel,
-		Subtopic:  msg.Subtopic,
-		Publisher: msg.Publisher,
-		Protocol:  msg.Protocol,
-		Created:   msg.Created,
-		Payload: map[string]interface{}{
-			"key1":      "val1",
-			"key2":      float64(123),
-			"key3":      "val3",
-			"key4/key5": "val5",
+	jsonMsg := json.Messages{
+		Data: []json.Message{
+			{
+				Channel:   msg.Channel,
+				Subtopic:  msg.Subtopic,
+				Publisher: msg.Publisher,
+				Protocol:  msg.Protocol,
+				Created:   msg.Created,
+				Payload: map[string]interface{}{
+					"key1":      "val1",
+					"key2":      float64(123),
+					"key3":      "val3",
+					"key4/key5": "val5",
+				},
+			},
 		},
-	}
-
-	jsonMsgs := json.Messages{
-		Data:   []json.Message{flatJSONMsg},
 		Format: msg.Subtopic,
 	}
 
@@ -94,8 +93,7 @@ func TestTransformJSON(t *testing.T) {
 		Format: msg.Subtopic,
 	}
 
-	// Test cases with JSON flattening.
-	flatCases := []struct {
+	cases := []struct {
 		desc string
 		msg  messaging.Message
 		json interface{}
@@ -104,7 +102,7 @@ func TestTransformJSON(t *testing.T) {
 		{
 			desc: "test transform JSON",
 			msg:  msg,
-			json: jsonMsgs,
+			json: jsonMsg,
 			err:  nil,
 		},
 		{
@@ -127,15 +125,54 @@ func TestTransformJSON(t *testing.T) {
 		},
 	}
 
-	noFlatJSONMsg := flatJSONMsg
-	noFlatJSONMsg.Payload = map[string]interface{}{
-		"key1": "val1",
-		"key2": float64(123),
-		"key3": "val3",
-		"key4": map[string]interface{}{"key5": "val5"},
+	for _, tc := range cases {
+		m, err := tr.Transform(tc.msg)
+		assert.Equal(t, tc.json, m, fmt.Sprintf("%s expected %v, got %v", tc.desc, tc.json, m))
+		assert.True(t, errors.Contains(err, tc.err), fmt.Sprintf("%s expected %s, got %s", tc.desc, tc.err, err))
+	}
+}
+
+func TestTransformerNested(t *testing.T) {
+	now := time.Now().Unix()
+	tr := json.New(false)
+
+	msg := messaging.Message{
+		Channel:   "channel-1",
+		Subtopic:  "subtopic-1",
+		Publisher: "publisher-1",
+		Protocol:  "protocol",
+		Payload:   []byte(validPayload),
+		Created:   now,
+	}
+	invalid := msg
+	invalid.Payload = []byte(invalidFlatPayload)
+
+	listMsg := msg
+	listMsg.Payload = []byte(listPayload)
+
+	jsonMsgs := json.Messages{
+		Data: []json.Message{
+			{
+				Channel:   msg.Channel,
+				Subtopic:  msg.Subtopic,
+				Publisher: msg.Publisher,
+				Protocol:  msg.Protocol,
+				Created:   msg.Created,
+				Payload: map[string]interface{}{
+					"key1": "val1",
+					"key2": float64(123),
+					"key3": "val3",
+					"key4": map[string]interface{}{"key5": "val5"},
+				},
+			},
+		},
+		Format: msg.Subtopic,
 	}
 
-	noFlatListJSON := json.Messages{
+	invalidFmt := msg
+	invalidFmt.Subtopic = ""
+
+	listJSON := json.Messages{
 		Data: []json.Message{
 			{
 				Channel:   msg.Channel,
@@ -167,12 +204,7 @@ func TestTransformJSON(t *testing.T) {
 		Format: msg.Subtopic,
 	}
 
-	jsonMsgs.Data = []json.Message{noFlatJSONMsg}
-	noFlatInvalid := invalid
-	noFlatInvalid.Payload = []byte(invalidFlatPayload)
-
-	// Test cases without JSON flattening.
-	noFlatCases := []struct {
+	cases := []struct {
 		desc string
 		msg  messaging.Message
 		json interface{}
@@ -193,25 +225,19 @@ func TestTransformJSON(t *testing.T) {
 		{
 			desc: "test no-flattening transform JSON array",
 			msg:  listMsg,
-			json: noFlatListJSON,
+			json: listJSON,
 			err:  nil,
 		},
 		{
 			desc: "test no-flattening transform JSON with invalid payload",
-			msg:  noFlatInvalid,
+			msg:  invalid,
 			json: nil,
 			err:  json.ErrTransform,
 		},
 	}
 
-	for _, tc := range flatCases {
-		m, err := trFlatten.Transform(tc.msg)
-		assert.Equal(t, tc.json, m, fmt.Sprintf("%s expected %v, got %v", tc.desc, tc.json, m))
-		assert.True(t, errors.Contains(err, tc.err), fmt.Sprintf("%s expected %s, got %s", tc.desc, tc.err, err))
-	}
-
-	for _, tc := range noFlatCases {
-		m, err := trNoFlatten.Transform(tc.msg)
+	for _, tc := range cases {
+		m, err := tr.Transform(tc.msg)
 		assert.Equal(t, tc.json, m, fmt.Sprintf("%s expected %v, got %v", tc.desc, tc.json, m))
 		assert.True(t, errors.Contains(err, tc.err), fmt.Sprintf("%s expected %s, got %s", tc.desc, tc.err, err))
 	}
