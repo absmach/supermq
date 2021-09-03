83c06d2d9795c0c0b3c256ec53ed2b5a6c9a7f84
fbugarski <filipbugarski@gmail.com>
2021-09-03 15:36:34 +0200

    Create a Commands Service
    Signed-off-by: fbugarski <filipbugarski@gmail.com>

---
+++ commands/api/metrics.go
@@ -0,0 +1,40 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+// +build !test
+
+package api
+
+import (
+	"time"
+
+	"github.com/go-kit/kit/metrics"
+	"github.com/mainflux/mainflux/commands"
+)
+
+var _ commands.Service = (*metricsMiddleware)(nil)
+
+type metricsMiddleware struct {
+	counter metrics.Counter
+	latency metrics.Histogram
+	svc     commands.Service
+}
+
+// MetricsMiddleware instruments core service by tracking request count and
+// latency.
+func MetricsMiddleware(svc commands.Service, counter metrics.Counter, latency metrics.Histogram) commands.Service {
+	return &metricsMiddleware{
+		counter: counter,
+		latency: latency,
+		svc:     svc,
+	}
+}
+
+func (ms *metricsMiddleware) Ping(secret string) (response string, err error) {
+	defer func(begin time.Time) {
+		ms.counter.With("method", "ping").Add(1)
+		ms.latency.With("method", "ping").Observe(time.Since(begin).Seconds())
+	}(time.Now())
+
+	return ms.svc.Ping(secret)
+}
