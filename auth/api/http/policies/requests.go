4a09922348ea1e36ab7c20631bbda659407e9888
Burak Sekili <buraksekili@gmail.com>
2021-10-22 14:22:36 +0300

    NOISSUE - Add tests for AddPolicies (#1480)
    * NOISSUE - Add tests for adding policy and update authz check
    
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>
    
    * Add more tests and update request body validation
    
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>
    
    * Update test case structure and utilize mock prefix for test ids
    
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- auth/api/http/policies/requests.go
+++ auth/api/http/policies/requests.go
@@ -1,6 +1,40 @@
 package policies
 
-import "github.com/mainflux/mainflux/auth"
+import (
+	"github.com/mainflux/mainflux/auth"
+	"github.com/mainflux/mainflux/things"
+)
+
+// Action represents an enum for the policies used in the Mainflux.
+type Action int
+
+const (
+	Create Action = iota
+	Read
+	Write
+	Delete
+	Access
+	Member
+	unknown
+)
+
+var actions = [...]string{
+	Create: "create",
+	Read:   "read",
+	Write:  "write",
+	Delete: "delete",
+	Access: "access",
+	Member: "member",
+}
+
+func parsePolicy(incomingAction string) Action {
+	for i, action := range actions {
+		if incomingAction == action {
+			return Action(i)
+		}
+	}
+	return unknown
+}
 
 type createPolicyReq struct {
 	token      string
@@ -16,6 +50,18 @@ func (req createPolicyReq) validate() error {
 
 	if len(req.SubjectIDs) == 0 || len(req.Policies) == 0 || req.Object == "" {
 		return auth.ErrMalformedEntity
+	}
+
+	for _, policy := range req.Policies {
+		if action := parsePolicy(policy); action > Member || action < Create {
+			return things.ErrMalformedEntity
+		}
+	}
+
+	for _, subject := range req.SubjectIDs {
+		if subject == "" {
+			return things.ErrMalformedEntity
+		}
 	}
 
 	return nil
