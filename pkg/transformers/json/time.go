b348e309dc03c8b240054accbeba4137cdb1cb07
Manuel Imperiale <manuel.imperiale@gmail.com>
2021-12-08 13:01:41 +0100

    Fix review
    Signed-off-by: Manuel Imperiale <manuel.imperiale@gmail.com>

--- pkg/transformers/json/time.go
+++ pkg/transformers/json/time.go
@@ -12,7 +12,7 @@ import (
 	"github.com/mainflux/mainflux/pkg/errors"
 )
 
-var errUnsupportedType = errors.New("unsupported type")
+var errUnsupportedFormat = errors.New("unsupported time format")
 
 func parseTimestamp(format string, timestamp interface{}, location string) (time.Time, error) {
 	switch format {
@@ -42,7 +42,7 @@ func parseUnix(format string, timestamp interface{}) (time.Time, error) {
 	case "unix_ns":
 		return time.Unix(0, integer).UTC(), nil
 	default:
-		return time.Unix(0, 0), errUnsupportedType
+		return time.Unix(0, 0), errUnsupportedFormat
 	}
 }
 
@@ -87,7 +87,7 @@ func parseComponents(timestamp interface{}) (int64, int64, error) {
 		integer, fractional := math.Modf(ts)
 		return int64(integer), int64(fractional * 1e9), nil
 	default:
-		return 0, 0, errUnsupportedType
+		return 0, 0, errUnsupportedFormat
 	}
 }
 
@@ -147,6 +147,6 @@ func parseTime(format string, timestamp interface{}, location string) (time.Time
 		}
 		return time.ParseInLocation(format, ts, loc)
 	default:
-		return time.Unix(0, 0), errUnsupportedType
+		return time.Unix(0, 0), errUnsupportedFormat
 	}
 }
