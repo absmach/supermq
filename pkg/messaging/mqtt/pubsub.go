6076dbdb2888e31d772d5a584a87a54ad4011c21
aryan <aryangodara03@gmail.com>
2022-08-12 16:41:34 +0530

    remove unnecessary mutex lock
    Signed-off-by: aryan <aryangodara03@gmail.com>

--- pkg/messaging/mqtt/pubsub.go
+++ pkg/messaging/mqtt/pubsub.go
@@ -87,7 +87,6 @@ func (ps pubsub) Subscribe(id, topic string, handler messaging.MessageHandler) e
 			if err != nil {
 				return err
 			}
-			ps.mu.Lock()
 			if len(ps.subscriptions) == 0 {
 				client, err := newClient(ps.address, id, ps.timeout)
 				if err != nil {
