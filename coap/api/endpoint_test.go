1d10706445eabfe463ab202bf464df6a67988445
aryan <aryangodara03@gmail.com>
2022-08-17 20:41:47 +0530

    rename transport_test to endpoint_test
    Signed-off-by: aryan <aryangodara03@gmail.com>

---
+++ coap/api/endpoint_test.go
@@ -0,0 +1,24 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+package api_test
+
+import (
+	"github.com/mainflux/mainflux"
+	"github.com/mainflux/mainflux/coap"
+	"github.com/mainflux/mainflux/coap/mocks"
+)
+
+const (
+	chanID   = "1"
+	id       = "1"
+	thingKey = "thing_key"
+	protocol = "coap"
+)
+
+var msg = []byte(`[{"n":"current","t":-1,"v":1.6}]`)
+
+func newService(cc mainflux.ThingsServiceClient) coap.Service {
+	pubsub := mocks.NewPubSub()
+	return coap.New(cc, pubsub)
+}
