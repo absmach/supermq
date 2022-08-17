098c9072aa5360b60e411da0c0b07bbca422b01e
aryan <aryangodara03@gmail.com>
2022-08-17 18:55:10 +0530

    initial commit
    Signed-off-by: aryan <aryangodara03@gmail.com>

---
+++ coap/adapter_test.go
@@ -0,0 +1,12 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+package coap_test
+
+const (
+	chanID   = "1"
+	id       = "1"
+	thingKey = "thing_key"
+	subTopic = "subtopic"
+	protocol = "coap"
+)
