201f3d0d78c1c95e4d5ef7dd05fc084ac636bd91
aryan <aryangodara03@gmail.com>
2022-09-13 10:56:37 +0530

    partial testHandle
    Signed-off-by: aryan <aryangodara03@gmail.com>

--- coap/client_test.go
+++ coap/client_test.go
@@ -4,9 +4,16 @@
 package coap_test
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"testing"
 
 	"github.com/mainflux/mainflux/coap"
+	go_coap "github.com/plgd-dev/go-coap/v2"
+	"github.com/plgd-dev/go-coap/v2/mux"
+	udp "github.com/plgd-dev/go-coap/v2/udp"
+	"github.com/stretchr/testify/assert"
 )
 
 const expectedCount = uint64(1)
@@ -17,10 +24,22 @@ var (
 	count   uint64
 )
 
-func TestHandle(t *testing.T) {
+func handler(w mux.ResponseWriter, r *mux.Message) {
 
 }
 
-func TestCancel(t *testing.T) {
+func TestHandle(t *testing.T) {
+	// server code
+	r := mux.NewRouter()
+	r.Handle("/", mux.HandlerFunc(handler))
+	log.Fatal(go_coap.ListenAndServe("udp", ":5688", r))
 
+	// client code
+	conn, err := udp.Dial("localhost:5688")
+	assert.Nil(t, err, fmt.Sprintf("expected nil error, got: %s\n", err))
+
+	resp, err := conn.Client().Get(context.Background(), "")
+	assert.Nil(t, err, fmt.Sprintf("expected nil error, got: %s\n", err))
+
+	log.Printf("Response Payload: %v\n", resp.String())
 }
