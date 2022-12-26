8ebd8d95d59bf5412e019d058cef267704e840ac
Arvindh <arvindh91@gmail.com>
2022-12-26 13:19:35 +0530

    unify : cassandra writer
    Signed-off-by: Arvindh <arvindh91@gmail.com>

--- cmd/cassandra-writer/main.go
+++ cmd/cassandra-writer/main.go
@@ -8,15 +8,14 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"strings"
 
 	"github.com/gocql/gocql"
-	"github.com/mainflux/mainflux"
 	"github.com/mainflux/mainflux/consumers"
 	"github.com/mainflux/mainflux/consumers/writers/api"
 	"github.com/mainflux/mainflux/consumers/writers/cassandra"
 	"github.com/mainflux/mainflux/internal"
+	cassandraClient "github.com/mainflux/mainflux/internal/client/cassandra"
+	"github.com/mainflux/mainflux/internal/env"
 	"github.com/mainflux/mainflux/internal/server"
 	httpserver "github.com/mainflux/mainflux/internal/server/http"
 	"github.com/mainflux/mainflux/logger"
@@ -25,45 +24,28 @@ import (
 )
 
 const (
-	svcName = "cassandra-writer"
-	sep     = ","
-
-	defBrokerURL  = "nats://localhost:4222"
-	defLogLevel   = "error"
-	defPort       = "8180"
-	defCluster    = "127.0.0.1"
-	defKeyspace   = "mainflux"
-	defDBUser     = "mainflux"
-	defDBPass     = "mainflux"
-	defDBPort     = "9042"
-	defConfigPath = "/config.toml"
-
-	envBrokerURL  = "MF_BROKER_URL"
-	envLogLevel   = "MF_CASSANDRA_WRITER_LOG_LEVEL"
-	envPort       = "MF_CASSANDRA_WRITER_PORT"
-	envCluster    = "MF_CASSANDRA_WRITER_DB_CLUSTER"
-	envKeyspace   = "MF_CASSANDRA_WRITER_DB_KEYSPACE"
-	envDBUser     = "MF_CASSANDRA_WRITER_DB_USER"
-	envDBPass     = "MF_CASSANDRA_WRITER_DB_PASS"
-	envDBPort     = "MF_CASSANDRA_WRITER_DB_PORT"
-	envConfigPath = "MF_CASSANDRA_WRITER_CONFIG_PATH"
+	svcName       = "cassandra-writer"
+	sep           = ","
+	envPrefix     = "MF_CASSANDRA_WRITER_"
+	envPrefixHttp = "MF_CASSANDRA_WRITER_HTTP_"
 )
 
 type config struct {
-	brokerURL  string
-	logLevel   string
-	port       string
-	configPath string
-	dbCfg      cassandra.DBConfig
+	brokerURL  string `env:"MF_BROKER_URL"     default:"nats://localhost:4222" `
+	logLevel   string `env:"MF_CASSANDRA_WRITER_LOG_LEVEL"     default:"debug" `
+	configPath string `env:"MF_CASSANDRA_WRITER_CONFIG_PATH"     default:"/config.toml" `
 }
 
 func main() {
-
 	ctx, cancel := context.WithCancel(context.Background())
 	g, ctx := errgroup.WithContext(ctx)
 
-	// create cassandra writer service configurations
-	cfg := loadConfig()
+	// create new cassandra writer service configurations
+	cfg := config{}
+	// load cassandra writer service configurations from environment
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("Failed to load %s service configuration : %s", svcName, err.Error())
+	}
 
 	// create new logger
 	logger, err := logger.New(os.Stdout, cfg.logLevel)
@@ -71,72 +53,55 @@ func main() {
 		log.Fatalf(err.Error())
 	}
 
+	///////////////// CASSANDRA CLIENT /////////////////////////
+	// create new cassandra config
+	cassandraConfig := cassandraClient.Config{}
+	// load cassandra config from environment
+	if err := env.Parse(&cassandraConfig, env.Options{Prefix: envPrefix}); err != nil {
+		log.Fatalf("Failed to load Cassandra database configuration : %s", err.Error())
+	}
+	// create new to cassandra client
+	cassaSession, err := cassandraClient.Connect(cassandraConfig)
+	if err != nil {
+		log.Fatalf("Failed to connect to Cassandra database : %s", err.Error())
+	}
+	defer cassaSession.Close()
+
+	////////// CASSANDRA WRITER REPO /////////////
+	repo := newService(cassaSession, logger)
+
+	// create new pub sub broker
 	pubSub, err := brokers.NewPubSub(cfg.brokerURL, "", logger)
 	if err != nil {
-		logger.Error(fmt.Sprintf("Failed to connect to message broker: %s", err))
-		os.Exit(1)
+		log.Fatalf("Failed to connect to message broker: %s", err)
 	}
 	defer pubSub.Close()
-
-	session := connectToCassandra(cfg.dbCfg, logger)
-	defer session.Close()
-
-	repo := newService(session, logger)
-
+	// Start consumer
 	if err := consumers.Start(svcName, pubSub, repo, cfg.configPath, logger); err != nil {
-		if err := consumers.Start(svcName, pubSub, repo, cfg.configPath, logger); err != nil {
-			logger.Error(fmt.Sprintf("Failed to create Cassandra writer: %s", err))
-		}
-
-		hs := httpserver.New(ctx, cancel, svcName, "", cfg.port, api.MakeHandler(svcName), "", "", logger)
-		g.Go(func() error {
-			return hs.Start()
-		})
-
-		g.Go(func() error {
-			return server.StopSignalHandler(ctx, cancel, logger, svcName, hs)
-		})
-
-		if err := g.Wait(); err != nil {
-			logger.Error(fmt.Sprintf("Cassandra writer service terminated: %s", err))
-		}
+		logger.Error(fmt.Sprintf("Failed to create Cassandra writer: %s", err))
 	}
+
+	///////////////// HTTP SERVER //////////////////////////
+	// create new http server config
+	httpServerConfig := server.Config{}
+	// load http server config from environment variables
+	if err := env.Parse(&httpServerConfig, env.Options{Prefix: envPrefix, AltPrefix: envPrefixHttp}); err != nil {
+		log.Fatalf(fmt.Sprintf("Failed to load %s HTTP server configuration : %s", svcName, err.Error()))
+	}
+	// create new http server
+	hs := httpserver.New(ctx, cancel, svcName, httpServerConfig, api.MakeHandler(svcName), logger)
+	g.Go(func() error {
+		return hs.Start()
+	})
+
+	g.Go(func() error {
+		return server.StopSignalHandler(ctx, cancel, logger, svcName, hs)
+	})
+
 	if err := g.Wait(); err != nil {
 		logger.Error(fmt.Sprintf("Cassandra writer service terminated: %s", err))
 	}
-}
 
-func loadConfig() config {
-	dbPort, err := strconv.Atoi(mainflux.Env(envDBPort, defDBPort))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dbCfg := cassandra.DBConfig{
-		Hosts:    strings.Split(mainflux.Env(envCluster, defCluster), sep),
-		Keyspace: mainflux.Env(envKeyspace, defKeyspace),
-		User:     mainflux.Env(envDBUser, defDBUser),
-		Pass:     mainflux.Env(envDBPass, defDBPass),
-		Port:     dbPort,
-	}
-
-	return config{
-		brokerURL:  mainflux.Env(envBrokerURL, defBrokerURL),
-		logLevel:   mainflux.Env(envLogLevel, defLogLevel),
-		port:       mainflux.Env(envPort, defPort),
-		configPath: mainflux.Env(envConfigPath, defConfigPath),
-		dbCfg:      dbCfg,
-	}
-}
-
-func connectToCassandra(dbCfg cassandra.DBConfig, logger logger.Logger) *gocql.Session {
-	session, err := cassandra.Connect(dbCfg)
-	if err != nil {
-		logger.Error(fmt.Sprintf("Failed to connect to Cassandra cluster: %s", err))
-		os.Exit(1)
-	}
-
-	return session
 }
 
 func newService(session *gocql.Session, logger logger.Logger) consumers.Consumer {
