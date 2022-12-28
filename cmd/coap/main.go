fcdd8c2df668cd9c00a27593f93b1e5fa9007cfc
Arvindh <arvindh91@gmail.com>
2022-12-28 15:35:10 +0530

    unify coap main.go
    Signed-off-by: Arvindh <arvindh91@gmail.com>

--- cmd/coap/main.go
+++ cmd/coap/main.go
@@ -8,78 +8,58 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
-	"time"
 
-	"github.com/mainflux/mainflux"
 	"github.com/mainflux/mainflux/coap"
 	"github.com/mainflux/mainflux/coap/api"
 	"github.com/mainflux/mainflux/internal"
-	internalauth "github.com/mainflux/mainflux/internal/auth"
+	thingsClient "github.com/mainflux/mainflux/internal/client/grpc/things"
+	"github.com/mainflux/mainflux/internal/env"
 	"github.com/mainflux/mainflux/internal/server"
 	coapserver "github.com/mainflux/mainflux/internal/server/coap"
 	httpserver "github.com/mainflux/mainflux/internal/server/http"
 	logger "github.com/mainflux/mainflux/logger"
 	"github.com/mainflux/mainflux/pkg/messaging/brokers"
-	thingsapi "github.com/mainflux/mainflux/things/api/auth/grpc"
 	"golang.org/x/sync/errgroup"
 )
 
 const (
-	svcName = "coap_adapter"
-
-	defPort              = "5683"
-	defBrokerURL         = "nats://localhost:4222"
-	defLogLevel          = "error"
-	defClientTLS         = "false"
-	defCACerts           = ""
-	defJaegerURL         = ""
-	defThingsAuthURL     = "localhost:8183"
-	defThingsAuthTimeout = "1s"
-
-	envPort              = "MF_COAP_ADAPTER_PORT"
-	envBrokerURL         = "MF_BROKER_URL"
-	envLogLevel          = "MF_COAP_ADAPTER_LOG_LEVEL"
-	envClientTLS         = "MF_COAP_ADAPTER_CLIENT_TLS"
-	envCACerts           = "MF_COAP_ADAPTER_CA_CERTS"
-	envJaegerURL         = "MF_JAEGER_URL"
-	envThingsAuthURL     = "MF_THINGS_AUTH_GRPC_URL"
-	envThingsAuthTimeout = "MF_THINGS_AUTH_GRPC_TIMEOUT"
+	svcName       = "coap_adapter"
+	envPrefix     = "MF_COAP_ADAPTER_"
+	envPrefixHttp = "MF_COAP_ADAPTER_HTTP_"
+	envPrefixCoap = "MF_COAP_ADAPTER_COAP_"
 )
 
 type config struct {
-	port              string
-	brokerURL         string
-	logLevel          string
-	clientTLS         bool
-	caCerts           string
-	jaegerURL         string
-	thingsAuthURL     string
-	thingsAuthTimeout time.Duration
+	logLevel  string `env:"MF_INFLUX_READER_LOG_LEVEL"  envDefault:"debug"`
+	brokerURL string `env:"MF_BROKER_URL"               envDefault:"nats://localhost:4222"`
+	jaegerURL string `env:"MF_JAEGER_URL"               envDefault:""`
 }
 
 func main() {
-	cfg := loadConfig()
 	ctx, cancel := context.WithCancel(context.Background())
 	g, ctx := errgroup.WithContext(ctx)
+
+	cfg := config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("Failed to load %s configuration : %s", svcName, err.Error())
+	}
 
 	logger, err := logger.New(os.Stdout, cfg.logLevel)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
-	conn := internalauth.ConnectToThings(cfg.clientTLS, cfg.caCerts, cfg.thingsAuthURL, svcName, logger)
-	defer conn.Close()
-
-	thingsTracer, thingsCloser := internalauth.Jaeger("things", cfg.jaegerURL, logger)
-	defer thingsCloser.Close()
-
-	tc := thingsapi.NewClient(conn, thingsTracer, cfg.thingsAuthTimeout)
+	tc, thingsGrpcClient, thingsTracerCloser, thingsGrpcSecure, err := thingsClient.Setup(envPrefix, cfg.jaegerURL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer thingsGrpcClient.Close()
+	defer thingsTracerCloser.Close()
+	logger.Info("Successfully connected to things grpc server " + thingsGrpcSecure)
 
 	nps, err := brokers.NewPubSub(cfg.brokerURL, "", logger)
 	if err != nil {
-		logger.Error(fmt.Sprintf("Failed to connect to message broker: %s", err))
-		os.Exit(1)
+		log.Fatalf("Failed to connect to message broker: %s", err.Error())
 	}
 	defer nps.Close()
 
@@ -90,8 +70,18 @@ func main() {
 	counter, latency := internal.MakeMetrics(svcName, "api")
 	svc = api.MetricsMiddleware(svc, counter, latency)
 
-	hs := httpserver.New(ctx, cancel, svcName, "", cfg.port, api.MakeHTTPHandler(), "", "", logger)
-	cs := coapserver.New(ctx, cancel, svcName, "", cfg.port, api.MakeCoAPHandler(svc, logger), logger)
+	httpServerConfig := server.Config{}
+	if err := env.Parse(&httpServerConfig, env.Options{Prefix: envPrefixHttp, AltPrefix: envPrefix}); err != nil {
+		log.Fatalf(fmt.Sprintf("failed to load %s HTTP server configuration : %s", svcName, err.Error()))
+	}
+	hs := httpserver.New(ctx, cancel, svcName, httpServerConfig, api.MakeHTTPHandler(), logger)
+
+	coapServerConfig := server.Config{}
+	if err := env.Parse(&httpServerConfig, env.Options{Prefix: envPrefixCoap, AltPrefix: envPrefix}); err != nil {
+		log.Fatalf(fmt.Sprintf("failed to load %s HTTP server configuration : %s", svcName, err.Error()))
+	}
+	cs := coapserver.New(ctx, cancel, svcName, coapServerConfig, api.MakeCoAPHandler(svc, logger), logger)
+
 	g.Go(func() error {
 		return hs.Start()
 	})
@@ -104,28 +94,5 @@ func main() {
 
 	if err := g.Wait(); err != nil {
 		logger.Error(fmt.Sprintf("CoAP adapter service terminated: %s", err))
-	}
-}
-
-func loadConfig() config {
-	tls, err := strconv.ParseBool(mainflux.Env(envClientTLS, defClientTLS))
-	if err != nil {
-		log.Fatalf("Invalid value passed for %s\n", envClientTLS)
-	}
-
-	authTimeout, err := time.ParseDuration(mainflux.Env(envThingsAuthTimeout, defThingsAuthTimeout))
-	if err != nil {
-		log.Fatalf("Invalid %s value: %s", envThingsAuthTimeout, err.Error())
-	}
-
-	return config{
-		brokerURL:         mainflux.Env(envBrokerURL, defBrokerURL),
-		port:              mainflux.Env(envPort, defPort),
-		logLevel:          mainflux.Env(envLogLevel, defLogLevel),
-		clientTLS:         tls,
-		caCerts:           mainflux.Env(envCACerts, defCACerts),
-		jaegerURL:         mainflux.Env(envJaegerURL, defJaegerURL),
-		thingsAuthURL:     mainflux.Env(envThingsAuthURL, defThingsAuthURL),
-		thingsAuthTimeout: authTimeout,
 	}
 }
