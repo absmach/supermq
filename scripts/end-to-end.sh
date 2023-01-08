4fb511113ed75da75b5993f91a60bcde45a55cfd
aryan <aryangodara03@gmail.com>
2023-01-08 14:37:51 +0530

    Cleaan up code. Use scripts variables
    Signed-off-by: aryan <aryangodara03@gmail.com>

--- scripts/end-to-end.sh
+++ scripts/end-to-end.sh
@@ -12,22 +12,16 @@ chmod 776 end-to-end.sh
 
 cd ..
 
-# ! Either check for the command first, if it exists, OR shift to using docker container for schemathesis
-# echo install schemathesis
-# pip install schemathesis
-
 echo running all docker containers now
 sudo make run
-echo have the containers started running
 
-echo "now provisioning for mf token"
+echo "now provisioning for MF_TOKEN"
 
 # 
 
 EMAIL=example@eg.com
 PASSWORD=12345678
 DEVICE=mf-device
-# CHANNEL=ch1
 
 #provision user:
 printf "Provisoning user with email $EMAIL and password $PASSWORD \n"
@@ -45,15 +39,8 @@ curl -s -S --cacert docker/ssl/certs/mainflux-server.crt --insecure -X POST -H "
 DEVICETOKEN=$(curl -s -S --cacert docker/ssl/certs/mainflux-server.crt --insecure -H "Authorization: Bearer $JWTTOKEN" https://localhost/things/1 | grep -Po "key\":\"\K(.*)(?=\")")
 printf "Device token is $DEVICETOKEN \n"
 
-# #provision channel
-# printf "Provisioning channel with name $CHANNEL \n"
-# curl -s -S --cacert docker/ssl/certs/mainflux-server.crt --insecure -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $JWTTOKEN" https://localhost/channels -d '{"name":"'"$CHANNEL"'"}'
-
-# #connect thing to channel
-# printf "Connecting thing to channel \n"
-# curl -s -S --cacert docker/ssl/certs/mainflux-server.crt --insecure -X PUT -H "Authorization: Bearer $JWTTOKEN" https://localhost/channels/1/things/1
-
-# 
+echo setting mf base path
+export MF_BASE_PATH=$(pwd)
 
 echo setting mf auth bearer token
 export MF_TOKEN=$JWTTOKEN
@@ -62,11 +49,12 @@ printf "Got the MF_TOKEN : $MF_TOKEN \n"
 
 #! TASK -> Automate below step instead of manually typing
 
-# SERVICES = auth bootstrap certs consumers-notifiers http provision readers things twins users websockets
-
 cd ./scripts
 make test
 cd -
+
+echo deleting the stapi container
+sudo docker rm stapi
 
 echo stopping the running containers
 sudo docker-compose -f docker/docker-compose.yml down
