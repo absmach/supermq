f82a4e69135a650656ac43f6c6b2a05c37a1db93
rodneyosodo <blackd0t@protonmail.com>
2023-02-24 00:35:24 +0300

    update sdk with new clients and groups for things and channels
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

--- pkg/sdk/go/requests.go
+++ pkg/sdk/go/requests.go
@@ -3,9 +3,17 @@
 
 package sdk
 
-type assignRequest struct {
-	Type    string   `json:"type,omitempty"`
-	Members []string `json:"members"`
+// updateClientSecretReq is used to update the client secret
+type updateClientSecretReq struct {
+	OldSecret string `json:"old_secret,omitempty"`
+	NewSecret string `json:"new_secret,omitempty"`
+}
+
+// updateClientIdentityReq is used to update the client identity
+type updateClientIdentityReq struct {
+	token    string
+	id       string
+	Identity string `json:"identity,omitempty"`
 }
 
 // UserPasswordReq contains old and new passwords
@@ -18,6 +26,7 @@ type UserPasswordReq struct {
 type ConnectionIDs struct {
 	ChannelIDs []string `json:"channel_ids"`
 	ThingIDs   []string `json:"thing_ids"`
+	Actions    []string `json:"actions,omitempty"`
 }
 
 type tokenReq struct {
