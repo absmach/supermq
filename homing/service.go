b03ebece81c242950150146962884a16a2b9943f
SammyOina <sammyoina@gmail.com>
2023-03-21 16:38:38 +0300

    add telemetry endpoints
    Signed-off-by: SammyOina <sammyoina@gmail.com>

--- homing/service.go
+++ homing/service.go
@@ -1,1 +1,8 @@
 package homing
+
+import "context"
+
+type Service interface {
+	Save(ctx context.Context, t Telemetry, serviceName string) error
+	GetAll(ctx context.Context, token string, pm PageMetadata) (TelemetryPage, error)
+}
