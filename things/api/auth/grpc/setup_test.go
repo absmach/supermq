5ac65589179807eea4599bdd36b5572c97e2a66e
aryan <aryangodara03@gmail.com>
2023-04-13 03:26:53 +0530

    fix grpc setup down to single simple function
    Signed-off-by: aryan <aryangodara03@gmail.com>

--- things/api/auth/grpc/setup_test.go
+++ things/api/auth/grpc/setup_test.go
@@ -30,54 +30,30 @@ var svc things.Service
 
 func TestMain(m *testing.M) {
 	serverErr := make(chan error)
-	done := make(chan interface{}, 1)
 
-	server := startGRPCServer(serverErr, done)
-
-	go func() {
-		for {
-			select {
-			case <-done:
-				return
-			case err := <-serverErr:
-				if err != nil {
-					log.Fatalln("gPRC Server Terminated : ", err)
-				}
-			}
-		}
-	}()
-
-	code := m.Run()
-	done <- true
-	done <- true
-
-	server.Stop()
-	os.Exit(code)
-}
-
-func startGRPCServer(serverErr chan error, done chan interface{}) *grpc.Server {
-	svc = newService(map[string]string{token: email})
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("got unexpected error while creating new listerner: %s", err)
 	}
 
+	svc = newService(map[string]string{token: email})
 	server := grpc.NewServer()
 	mainflux.RegisterThingsServiceServer(server, grpcapi.NewServer(mocktracer.New(), svc))
 
-	go func(done chan interface{}, server *grpc.Server) {
-		for {
-			select {
-			case serverErr <- server.Serve(listener):
-				return
-			case <-done:
-				return
-			}
+	// Start gRPC server in detached mode.
+	go func() {
+		serverErr <- server.Serve(listener)
+	}()
 
-		}
-	}(done, server)
+	code := m.Run()
 
-	return server
+	server.GracefulStop()
+	err = <-serverErr
+	if err != nil {
+		log.Fatalln("gPRC Server Terminated : ", err)
+	}
+	close(serverErr)
+	os.Exit(code)
 }
 
 func newService(tokens map[string]string) things.Service {
