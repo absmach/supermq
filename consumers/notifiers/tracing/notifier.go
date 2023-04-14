9e72029bf37e858b7cd7489471c35bb1763f8067
SammyOina <sammyoina@gmail.com>
2023-04-14 13:16:51 +0300

    reorder functions
    Signed-off-by: SammyOina <sammyoina@gmail.com>

--- consumers/notifiers/tracing/notifier.go
+++ consumers/notifiers/tracing/notifier.go
@@ -16,18 +16,18 @@ type serviceMiddleware struct {
 	tracer opentracing.Tracer
 }
 
-// Notify implements notifiers.Notifier
-func (sm *serviceMiddleware) Notify(from string, to []string, msg *messaging.Message) error {
-	span := sm.tracer.StartSpan(notifierOp, ext.SpanKindConsumer)
-	ext.MessageBusDestination.Set(span, msg.Subtopic)
-	defer span.Finish()
-	return sm.svc.Notify(from, to, msg)
-}
-
 // NewNotifier create new notifier tracing middleware service
 func NewNotifier(svc notifiers.Notifier, tracer opentracing.Tracer) notifiers.Notifier {
 	return &serviceMiddleware{
 		svc:    svc,
 		tracer: tracer,
 	}
+}
+
+// Notify implements notifiers.Notifier
+func (sm *serviceMiddleware) Notify(from string, to []string, msg *messaging.Message) error {
+	span := sm.tracer.StartSpan(notifierOp, ext.SpanKindConsumer)
+	ext.MessageBusDestination.Set(span, msg.Subtopic)
+	defer span.Finish()
+	return sm.svc.Notify(from, to, msg)
 }
