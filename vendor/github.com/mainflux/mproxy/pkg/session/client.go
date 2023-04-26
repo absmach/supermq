a585b419598727b4e36965e3f3ae40d8dd047602
SammyOina <sammyoina@gmail.com>
2023-04-26 16:09:53 +0300

    update mproxy
    Signed-off-by: SammyOina <sammyoina@gmail.com>

--- vendor/github.com/mainflux/mproxy/pkg/session/client.go
+++ vendor/github.com/mainflux/mproxy/pkg/session/client.go
@@ -6,10 +6,11 @@ import (
 	"errors"
 )
 
-type ctxkey string
+type ctxkey int
 
-const MPROXY_CLIENT_KEY ctxkey = "mproxy-client"
+const mproxyClientKey ctxkey = 1
 
+// ErrClientNotInContext failed to retrieve client from context
 var ErrClientNotInContext error = errors.New("client not set in context")
 
 // Client stores MQTT client data.
@@ -20,12 +21,14 @@ type Client struct {
 	Cert     x509.Certificate
 }
 
+// ToContext store Client in context.Context values
 func (c Client) ToContext(ctx context.Context) context.Context {
-	return context.WithValue(ctx, MPROXY_CLIENT_KEY, c)
+	return context.WithValue(ctx, mproxyClientKey, c)
 }
 
+// FromContext retrieve client from context.Context
 func (c *Client) FromContext(ctx context.Context) error {
-	if client, ok := ctx.Value(MPROXY_CLIENT_KEY).(Client); ok {
+	if client, ok := ctx.Value(mproxyClientKey).(Client); ok {
 		c.ID = client.ID
 		c.Password = client.Password
 		c.Username = client.Username
