8390e695a601adc30b44dc3d76af74eb293131e6
SammyOina <sammyoina@gmail.com>
2023-05-16 00:10:20 +0300

    fix retrieve all tests
    Signed-off-by: SammyOina <sammyoina@gmail.com>

--- things/clients/postgres/clients_test.go
+++ things/clients/postgres/clients_test.go
@@ -256,7 +256,6 @@ func TestClientsRetrieveAll(t *testing.T) {
 		_, err = prepo.Save(context.Background(), policy)
 		require.Nil(t, err, fmt.Sprintf("unexpected error: %s", err))
 	}
-	fmt.Println("client3", expectedClients[3])
 
 	cases := map[string]struct {
 		size     uint64
