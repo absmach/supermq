c5106baa87359a49b4409170a6c177351a2d86db
Rodney Osodo <socials@rodneyosodo.com>
2023-09-11 20:07:44 +0300

    Add RabbitMQ event publisher implementation
    This commit adds the implementation of the RabbitMQ event publisher.
    
    The RabbitMQ event publisher is responsible for publishing events
    to a RabbitMQ messaging system. It uses the `messaging`
    package to interact with the messaging system.
    
    The implementation includes the following features:
    
    - Publishing events to RabbitMQ using the `Publish` method.
    - Marshaling events to JSON before publishing.
    - Setting the message subject and headers based on the event.
    - Handling errors during publishing.
    
    This implementation is built with the `rabbitmq` build tag,
    which means it will only be compiled if the `rabbitmq` build tag
    is present.
    
    The RabbitMQ event publisher is part of the Mainflux events package
    and provides support for the Mainflux RabbitMQ events source service functionality.
    
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>
    Signed-off-by: Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

---
+++ pkg/events/rabbitmq/publisher.go
@@ -0,0 +1,129 @@
+//go:build rabbitmq
+// +build rabbitmq
+
+package rabbitmq
+
+import (
+	"context"
+	"encoding/json"
+	"sync"
+	"time"
+
+	"github.com/mainflux/mainflux/pkg/events"
+	"github.com/mainflux/mainflux/pkg/messaging"
+	"github.com/mainflux/mainflux/pkg/messaging/brokers"
+	broker "github.com/mainflux/mainflux/pkg/messaging/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type pubEventStore struct {
+	conn              *amqp.Connection
+	publisher         messaging.Publisher
+	unpublishedEvents chan *messaging.Message
+	stream            string
+	mu                sync.Mutex
+}
+
+func NewPublisher(ctx context.Context, url, stream string) (events.Publisher, error) {
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	if err := ch.ExchangeDeclare(exchangeName, amqp.ExchangeTopic, true, false, false, false, nil); err != nil {
+		return nil, err
+	}
+
+	publisher, err := broker.NewPublisher(url, brokers.WithPrefix(&eventsPrefix), brokers.WithChannel(ch))
+	if err != nil {
+		return nil, err
+	}
+
+	es := &pubEventStore{
+		conn:              conn,
+		publisher:         publisher,
+		unpublishedEvents: make(chan *messaging.Message, events.MaxUnpublishedEvents),
+		stream:            stream,
+	}
+
+	go es.StartPublishingRoutine(ctx)
+
+	return es, nil
+}
+
+func (es *pubEventStore) Publish(ctx context.Context, event events.Event) error {
+	values, err := event.Encode()
+	if err != nil {
+		return err
+	}
+	values["occurred_at"] = time.Now().UnixNano()
+
+	data, err := json.Marshal(values)
+	if err != nil {
+		return err
+	}
+
+	var record = &messaging.Message{
+		Payload: data,
+	}
+
+	if ok := es.checkConnection(ctx); !ok {
+		es.mu.Lock()
+		defer es.mu.Unlock()
+
+		select {
+		case es.unpublishedEvents <- record:
+		default:
+			// If the channel is full (rarely happens), drop the events.
+			return nil
+		}
+
+		return nil
+	}
+
+	return es.publisher.Publish(ctx, es.stream, record)
+}
+
+func (es *pubEventStore) StartPublishingRoutine(ctx context.Context) {
+	defer close(es.unpublishedEvents)
+
+	ticker := time.NewTicker(events.UnpublishedEventsCheckInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			if ok := es.checkConnection(ctx); ok {
+				es.mu.Lock()
+				for i := len(es.unpublishedEvents) - 1; i >= 0; i-- {
+					record := <-es.unpublishedEvents
+					if err := es.publisher.Publish(ctx, es.stream, record); err != nil {
+						es.unpublishedEvents <- record
+
+						break
+					}
+				}
+				es.mu.Unlock()
+			}
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (es *pubEventStore) Close() error {
+	es.conn.Close()
+
+	return es.publisher.Close()
+}
+
+func (es *pubEventStore) checkConnection(ctx context.Context) bool {
+	// A timeout is used to avoid blocking the main thread
+	ctx, cancel := context.WithTimeout(ctx, events.ConnCheckInterval)
+	defer cancel()
+
+	return es.conn.IsClosed()
+}
---
+++ pkg/events/rabbitmq/setup_test.go
@@ -0,0 +1,82 @@
+// Copyright (c) Mainflux
+// SPDX-License-Identifier: Apache-2.0
+
+//go:build rabbitmq
+// +build rabbitmq
+
+
+package rabbitmq_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"testing"
+
+	"github.com/mainflux/mainflux/pkg/events/rabbitmq"
+	"github.com/ory/dockertest/v3"
+)
+
+var (
+	rabbitmqURL string
+	stream      = "tests.events"
+	consumer    = "tests-consumer"
+	ctx         = context.Background()
+)
+
+func TestMain(m *testing.M) {
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	container, err := pool.Run("rabbitmq", "3.9.20", []string{})
+	if err != nil {
+		log.Fatalf("Could not start container: %s", err)
+	}
+
+	handleInterrupt(pool, container)
+
+	rabbitmqURL = fmt.Sprintf("amqp://%s:%s", "localhost", container.GetPort("5672/tcp"))
+
+	fmt.Printf("address: %s\n", rabbitmqURL)
+	if err := pool.Retry(func() error {
+		_, err = rabbitmq.NewPublisher(ctx, rabbitmqURL, stream)
+		return err
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	if err := pool.Retry(func() error {
+		_, err = rabbitmq.NewSubscriber(ctx, rabbitmqURL, stream, consumer, logger)
+		return err
+	}); err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	fmt.Println("RabbitMQ started")
+
+	code := m.Run()
+
+	if err := pool.Purge(container); err != nil {
+		log.Fatalf("Could not purge container: %s", err)
+	}
+
+	os.Exit(code)
+}
+
+func handleInterrupt(pool *dockertest.Pool, container *dockertest.Resource) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-c
+		if err := pool.Purge(container); err != nil {
+			log.Fatalf("Could not purge container: %s", err)
+		}
+		os.Exit(0)
+	}()
+}
