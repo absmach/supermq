absmach/magistrala

Add event logs service
Signed-off-by: Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>

internal/api/common.go
@@ -130,7 +130,9 @@ func EncodeError(_ context.Context, err error, w http.ResponseWriter) {
 		errors.Contains(err, svcerr.ErrPasswordFormat),
 		errors.Contains(err, apiutil.ErrInvalidLevel),
 		errors.Contains(err, apiutil.ErrInvalidQueryParams),
-		errors.Contains(err, apiutil.ErrValidation):
+		errors.Contains(err, apiutil.ErrValidation),
+		errors.Contains(err, apiutil.ErrMissingEntityType),
+		errors.Contains(err, apiutil.ErrInvalidEntityType):
 		w.WriteHeader(http.StatusBadRequest)
 	case errors.Contains(err, svcerr.ErrAuthentication),
 		errors.Contains(err, svcerr.ErrLogin),

absmach/supermq

internal/api/common.go
@@ -154,7 +154,10 @@ func EncodeError(_ context.Context, err error, w http.ResponseWriter) {
 		errors.Contains(err, apiutil.ErrInvalidTopic),
 		errors.Contains(err, bootstrap.ErrAddBootstrap),
 		errors.Contains(err, apiutil.ErrInvalidCertData),
-		errors.Contains(err, apiutil.ErrEmptyMessage):
+		errors.Contains(err, apiutil.ErrEmptyMessage),
+		errors.Contains(err, apiutil.ErrInvalidLevel),
+		errors.Contains(err, apiutil.ErrMissingEntityType),
+		errors.Contains(err, apiutil.ErrInvalidEntityType):
 		err = unwrap(err)
 		w.WriteHeader(http.StatusBadRequest)
 