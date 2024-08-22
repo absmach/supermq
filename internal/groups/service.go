absmach/magistrala

feat(groups): add ltree to groups
Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

feat(groups): ffetch groups using ltree

Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

fix(groups): remove lquery

Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

feat(groups): add index

Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

fix(groups): handle empty response body

Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

refactor groups for test

Signed-off-by: WashingtonKK <washingtonkigan@gmail.com>

internal/groups/service.go
@@ -205,7 +205,13 @@ func (svc service) ListGroups(ctx context.Context, token, memberKind, memberID s
 	default:
 		return groups.Page{}, errMemberKind
 	}
-
+	if gm.ID != "" {
+		// Check if group exists.
+		_, err := svc.authorizeToken(ctx, auth.UserType, token, auth.ViewPermission, auth.GroupType, gm.ID)
+		if err != nil {
+			return groups.Page{}, errors.Wrap(svcerr.ErrViewEntity, err)
+		}
+	}
 	gp, err := svc.groups.RetrieveByIDs(ctx, gm, ids...)
 	if err != nil {
 		return groups.Page{}, errors.Wrap(svcerr.ErrViewEntity, err)

absmach/supermq

internal/groups/service.go
@@ -205,6 +205,13 @@ func (svc service) ListGroups(ctx context.Context, token, memberKind, memberID s
 	default:
 		return groups.Page{}, errMemberKind
 	}
+	if gm.ParentID != "" {
+		// Check if group exists.
+		_, err := svc.authorizeToken(ctx, auth.UserType, token, auth.ViewPermission, auth.GroupType, gm.ID)
+		if err != nil {
+			return groups.Page{}, errors.Wrap(svcerr.ErrViewEntity, err)
+		}
+	}
 	gp, err := svc.groups.RetrieveByIDs(ctx, gm, ids...)
 	if err != nil {
 		return groups.Page{}, errors.Wrap(svcerr.ErrViewEntity, err)