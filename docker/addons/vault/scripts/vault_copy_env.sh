7149f4cb4b49977e48ecf37020571b6c4b9b3886
JeffMboya <jangina.mboya@gmail.com>
2024-08-29 16:03:03 +0300

    Update Vault environment variables in vault_copy_env.sh script
    Signed-off-by: JeffMboya <jangina.mboya@gmail.com>
    
    Update Vault environment variables in vault_copy_env.sh script
    
    Signed-off-by: JeffMboya <jangina.mboya@gmail.com>

--- docker/addons/vault/scripts/vault_copy_env.sh
+++ docker/addons/vault/scripts/vault_copy_env.sh
@@ -30,10 +30,18 @@ write_env() {
     data_dir="$scriptdir/data"
 
     if [ -e "$data_dir/secrets" ]; then
-        sed -i "s,MG_VAULT_UNSEAL_KEY_1=.*,MG_VAULT_UNSEAL_KEY_1=$(awk -F ': ' '$1 == \"Unseal Key 1\" {print $2}' "$data_dir/secrets")," "$env_file"
-        sed -i "s,MG_VAULT_UNSEAL_KEY_2=.*,MG_VAULT_UNSEAL_KEY_2=$(awk -F ': ' '$1 == \"Unseal Key 2\" {print $2}' "$data_dir/secrets")," "$env_file"
-        sed -i "s,MG_VAULT_UNSEAL_KEY_3=.*,MG_VAULT_UNSEAL_KEY_3=$(awk -F ': ' '$1 == \"Unseal Key 3\" {print $2}' "$data_dir/secrets")," "$env_file"
-        sed -i "s,MG_VAULT_TOKEN=.*,MG_VAULT_TOKEN=$(awk -F ': ' '$1 == \"Initial Root Token\" {print $2}' "$data_dir/secrets")," "$env_file"
+        awk -F ': ' '/Unseal Key 1/ {print "MG_VAULT_UNSEAL_KEY_1=" $2}' "$data_dir/secrets" | sed -i 's,^MG_VAULT_UNSEAL_KEY_1=.*,'"$(
+            awk -F ': ' '/Unseal Key 1/ {print "MG_VAULT_UNSEAL_KEY_1=" $2}' "$data_dir/secrets"
+        )"',' "$env_file"
+        awk -F ': ' '/Unseal Key 2/ {print "MG_VAULT_UNSEAL_KEY_2=" $2}' "$data_dir/secrets" | sed -i 's,^MG_VAULT_UNSEAL_KEY_2=.*,'"$(
+            awk -F ': ' '/Unseal Key 2/ {print "MG_VAULT_UNSEAL_KEY_2=" $2}' "$data_dir/secrets"
+        )"',' "$env_file"
+        awk -F ': ' '/Unseal Key 3/ {print "MG_VAULT_UNSEAL_KEY_3=" $2}' "$data_dir/secrets" | sed -i 's,^MG_VAULT_UNSEAL_KEY_3=.*,'"$(
+            awk -F ': ' '/Unseal Key 3/ {print "MG_VAULT_UNSEAL_KEY_3=" $2}' "$data_dir/secrets"
+        )"',' "$env_file"
+        awk -F ': ' '/Initial Root Token/ {print "MG_VAULT_TOKEN=" $2}' "$data_dir/secrets" | sed -i 's,^MG_VAULT_TOKEN=.*,'"$(
+            awk -F ': ' '/Initial Root Token/ {print "MG_VAULT_TOKEN=" $2}' "$data_dir/secrets"
+        )"',' "$env_file"
         echo "Vault environment variables are set successfully in $env_file"
     else
         echo "Error: Source file '$data_dir/secrets' not found."
@@ -41,3 +49,4 @@ write_env() {
 }
 
 write_env
+
